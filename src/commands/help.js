c2f650478e370bb374e71c623773b334b2097a30
EricHoftiezer <erichoftiezer@gmail.com>
2022-04-20 18:15:09 -0500

    Minor fixes for production

--- src/commands/help.js
+++ src/commands/help.js
@@ -18,15 +18,7 @@ module.exports = {
         logger.info(`[${PREFIX}] Initialized by ${username} in ${channel} on ${guild}!`);
 
         const help_text = '**Global Commands**\n\
-*about:* Shows information about this bot.\n\
-*contact:* Show information on how to contact TripSit.\n\
-*info (Drug) (Summary|Dosage|Combos):* Displays drug information\n\
-*combo (Drug) (Drug):* Shows combo information between two substances\n\
-*breathe (1|2):* Breathing exercises in the air\n\
-*hydrate:* Reminder to hydrate!\n\
-*kipp:* Reminder to keep it positive please!\n\
 *topic:* Displays a random topic question\n\
-*idose:* (PM ONLY) Allows the user to record when they dosed a substance\n\
 \n\
 **Tripsit-Only Commands**\n\
 *tripsit (Member) (On|Off):* This will remove all roles from a user and add the NeedsHelp role, basically forcing the user into the #tripsit channel.\n\
@@ -37,7 +29,31 @@ module.exports = {
             .setAuthor({ name: 'TripSit.Me ', url: 'http://www.tripsit.me', iconURL: ts_icon_url })
             .setColor('RANDOM')
             .setTitle('TripBot Help')
-            .setDescription(help_text);
+            .setDescription(help_text)
+            .addFields(
+                { name: 'About', value: 'Information on Team TripSit and who built this bot.', inline: true },
+                { name: 'Contact', value: 'How to contact Team TripSit and the bot builder.', inline: true },
+                { name: 'Help', value: 'Information on all commands, you\'re here now!', inline: true },
+            )
+            .addFields(
+                { name: 'Info', value: 'This command looks up drug information!', inline: true },
+                { name: 'Combo', value: 'Checks the interactions between two drugs.', inline: true },
+                { name: 'iDose', value: 'This command can be used to remind yourself when you lasted consumed.', inline: true },
+            )
+            .addFields(
+                { name: 'Breathe', value: 'Remind people to breathe.', inline: true },
+                { name: 'Hydrate', value: 'Covert others to the hydro homie cause', inline: true },
+                { name: 'KIPP', value: 'Keep It Positive Please!', inline: true },
+            )
+            .addFields(
+                { name: 'Topic', value: 'Displays a random topic', inline: true },
+                { name: 'Bug', value: 'Sends a message to the development room on the TripSit server', inline: true },
+            )
+            .addFields(
+                { name: 'Karma', value: 'TripSit Only - Keeps track of karma (reactions) given and received.', inline: true },
+                { name: 'Mod', value: 'TripSit Only - Applies mod actions on a user.', inline: true },
+                { name: 'Report', value: 'TripSit Only - Allows users to report someone to the TripSit Team.', inline: true },
+            );
         return interaction.reply({ embeds: [embed] });
     },
 };
