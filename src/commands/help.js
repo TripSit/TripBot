761a8fa38e7b363143f8f6cbfb65046f09a91b3b
EricHoftiezer <erichoftiezer@gmail.com>
2022-04-20 18:42:41 -0500

    "one last fix"

--- src/commands/help.js
+++ src/commands/help.js
@@ -17,19 +17,10 @@ module.exports = {
         const guild = interaction.guild.name;
         logger.info(`[${PREFIX}] Initialized by ${username} in ${channel} on ${guild}!`);
 
-        const help_text = '**Global Commands**\n\
-*topic:* Displays a random topic question\n\
-\n\
-**Tripsit-Only Commands**\n\
-*tripsit (Member) (On|Off):* This will remove all roles from a user and add the NeedsHelp role, basically forcing the user into the #tripsit channel.\n\
-*tripsitme:* This is a button in the #tripsit room that will start a new thread in #tripsit to discuss your trip\n\
-*karma (Member):* This records the reactions (emojis) given and received, and displays the history of each user\n';
-
         const embed = new MessageEmbed()
             .setAuthor({ name: 'TripSit.Me ', url: 'http://www.tripsit.me', iconURL: ts_icon_url })
             .setColor('RANDOM')
             .setTitle('TripBot Help')
-            .setDescription(help_text)
             .addFields(
                 { name: 'About', value: 'Information on Team TripSit and who built this bot.', inline: true },
                 { name: 'Contact', value: 'How to contact Team TripSit and the bot builder.', inline: true },
