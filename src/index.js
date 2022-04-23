fe9bc4b8234efc808866debca041a85aee85206a
EricHoftiezer <erichoftiezer@gmail.com>
2022-04-23 12:51:07 -0500

    Adding convert to global commands

--- src/index.js
+++ src/index.js
@@ -46,9 +46,9 @@ const client = new Client({
 
 // Set up commands
 const guild_commands = [];
-const guild_command_names = ['issue', 'benzo_convert', 'botmod', 'tripsit', 'karma', 'tripsitme', 'report', 'mod', 'button', 'gban', 'gunban', 'uban', 'uunban', 'chitragupta', 'test'];
+const guild_command_names = ['issue', 'botmod', 'tripsit', 'karma', 'tripsitme', 'report', 'mod', 'button', 'gban', 'gunban', 'uban', 'uunban', 'chitragupta', 'test'];
 const globl_commands = [];
-const globl_command_names = ['dxmcalc', 'ems', 'recovery', 'help', 'bug', 'about', 'breathe', 'combo', 'contact', 'hydrate', 'info', 'kipp', 'topic', 'idose'];
+const globl_command_names = ['benzo_convert', 'dxmcalc', 'ems', 'recovery', 'help', 'bug', 'about', 'breathe', 'combo', 'contact', 'hydrate', 'info', 'kipp', 'topic', 'idose'];
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
