4493e8e13c99dfc548346b0ad76082b0ffe0cb31
Moonbear <teknos@tripsit.me>
2022-04-24 10:41:23 -0500

    Updated bug submit functionality

--- src/commands/bug.js
+++ src/commands/bug.js
@@ -2,9 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const logger = require('../utils/logger.js');
 const PREFIX = require('path').parse(__filename).name;
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
+if (process.env.NODE_ENV !== 'production') {require('dotenv').config();}
 const ts_icon_url = process.env.ts_icon_url;
 const channel_dev_id = process.env.channel_development;
 
@@ -23,20 +21,22 @@ module.exports = {
         const bug_report = interaction.options.getString('bug_report');
         logger.debug(`[${PREFIX}] bug_report: ${bug_report}`);
 
+        const bot_owner = interaction.client.users.cache.get(process.env.ownerId);
+
         logger.debug(`[${PREFIX}] channel_dev_id: ${channel_dev_id}`);
         const dev_chan = interaction.client.channels.cache.get(channel_dev_id);
         const dev_embed = new MessageEmbed()
             .setAuthor({ name: 'TripSit.Me ', url: 'http://www.tripsit.me', iconURL: ts_icon_url })
             .setColor('RANDOM')
-            .setTitle(`Bug report from ${username} in ${channel} on ${guild}`)
-            .setDescription(bug_report);
+            .setDescription(`Hey ${bot_owner.toString()},\n${username} on ${guild} reports:\n${bug_report}`)
+            .setFooter({ text: 'Good luck!' });
         dev_chan.send({ embeds: [dev_embed] });
 
         const embed = new MessageEmbed()
             .setAuthor({ name: 'TripSit.Me ', url: 'http://www.tripsit.me', iconURL: ts_icon_url })
             .setColor('RANDOM')
             .setTitle('Thank you!')
-            .setDescription('I\'ve submitted this feedback to the bot owner. \n\nYou\'re more than welcome to join the TripSit server and speak to Moonbear directly if you want!\nCheck the /contact command for more info.');
+            .setDescription('I\'ve submitted this feedback to the bot owner. \n\nYou\'re more than welcome to join the TripSit server and speak to Moonbear directly if you want! Check the /contact command for more info.');
         interaction.reply({ embeds: [embed], ephemeral: true });
         return;
     },
