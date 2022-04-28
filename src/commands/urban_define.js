a4fa34c58cb0c50dbbdfbc0ffd94c327f57e5eb9
EricHoftiezer <erichoftiezer@gmail.com>
2022-04-28 10:07:44 -0500

    Fixing api key in UD

--- src/commands/urban_define.js
+++ src/commands/urban_define.js
@@ -3,10 +3,8 @@ const { MessageEmbed } = require('discord.js');
 const logger = require('../utils/logger.js');
 const PREFIX = require('path').parse(__filename).name;
 const axios = require('axios');
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-const UD_TOKEN = process.env.urban_dict_key;
+if (process.env.NODE_ENV !== 'production') {require('dotenv').config();}
+const UD_TOKEN = process.env.rapid_api_key;
 const ts_icon_url = process.env.ts_icon_url;
 const ts_flame_url = process.env.ts_flame_url;
 
