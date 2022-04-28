c68bd94c2b3d6a35de5d0f88e856afb93c1d2f81
Moonbear <teknos@tripsit.me>
2022-04-28 14:28:32 -0500

    idk

--- src/commands/pill_id.js
+++ src/commands/pill_id.js
@@ -75,7 +75,8 @@ module.exports = {
         axios.request({
             method: 'GET',
             url: url,
-            headers: { 'User-Agent': 'Axios 0.21.1' },
+            headers: { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36' },
+            // headers: { 'User-Agent': 'Axios 0.21.1' },
         }).then(async function(response) {
             logger.debug(`[${PREFIX}] axios base request worked!`);
             const { document } = (new JSDOM(response.data, { includeNodeLocations: true })).window;
