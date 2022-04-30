249d1d1324583e3c6fb6357dfc8a707bbd643acc
Moonbear <teknos@tripsit.me>
2022-04-30 18:12:06 -0500

    Minor typo

--- src/commands/combochart.js
+++ src/commands/combochart.js
@@ -3,7 +3,7 @@ const logger = require('../utils/logger.js');
 const PREFIX = require('path').parse(__filename).name;
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('combochart').setDescription('Display TripSit\s Combo Chart'),
+    data: new SlashCommandBuilder().setName('combochart').setDescription('Display TripSit\'s Combo Chart'),
     async execute(interaction) {
         const url = 'https://i.imgur.com/juzYjDl.png';
         interaction.reply(url);
