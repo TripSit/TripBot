d7ed5d228ae8d1ba0e7524241bc1e46132bcbadd
EricHoftiezer <erichoftiezer@gmail.com>
2022-05-03 18:30:04 -0500

    Fixing an issue with time and karma

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,10 +1,16 @@
 const PREFIX = require('path').parse(__filename).name;
 const logger = require('../utils/logger.js');
+if (process.env.NODE_ENV !== 'production') {require('dotenv').config();}
+const guild_id = process.env.guildId;
 
 module.exports = {
     name: 'messageReactionAdd',
     execute(reaction, user, client) {
         logger.debug(`[${PREFIX}] Reaction added`);
+        // logger.debug(`[${PREFIX}] Reaction: ${JSON.stringify(reaction, null, 2)}`);
+        // logger.debug(`[${PREFIX}] User: ${JSON.stringify(user, null, 2)}`);
+        // logger.debug(`[${PREFIX}] Client: ${JSON.stringify(client, null, 2)}`);
+
         // logger.debug(`[${PREFIX}] reaction1: ${JSON.stringify(reaction, null, 4)}`);
         // When a reaction is received, check if the structure is partial
         if (reaction.partial) {
@@ -15,6 +21,7 @@ module.exports = {
                     // logger.debug(`[${PREFIX}] reaction2: ${JSON.stringify(reaction, null, 4)}`);
                     const reaction_author = reaction.message.author;
                     const reaction_emoji = reaction.emoji;
+                    if (reaction.message.guild.id !== guild_id) { return; }
                     logger.debug(`[${PREFIX}] ${user.username} gave ${reaction_emoji.name} to ${reaction_author.username} in ${reaction.message.guild}!`);
                     const command = client.commands.get('chitragupta');
                     command.execute('chitragupta', user, 1, reaction_emoji.toString(), reaction_author);
