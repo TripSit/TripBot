198349e37ef44abafad9b0061e4b48b1114bf78f
SevenCatsHere <sevencats@tripsit.me>
2022-05-04 09:13:21 -0400

    more command linting

--- src/commands/global/recovery.js
+++ src/commands/global/recovery.js
@@ -1,12 +1,20 @@
+'use strict';
+
+const path = require('path');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const logger = require('../../utils/logger');
-const PREFIX = require('path').parse(__filename).name;
+
+const PREFIX = path.parse(__filename).name;
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('recovery').setDescription('Information that may be helpful in a serious situation.'),
+  data: new SlashCommandBuilder()
+    .setName('recovery')
+    .setDescription('Information that may be helpful in a serious situation.'),
+
   async execute(interaction) {
     const url = 'https://i.imgur.com/nTEm0QE.png';
-    if (!interaction.replied) { interaction.reply(url); } else { interaction.followUp(url); }
+    if (!interaction.replied) interaction.reply(url);
+    else interaction.followUp(url);
     logger.debug(`[${PREFIX}] finished!`);
   },
 };
