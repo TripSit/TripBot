1c3dead4f94b513b4fc6e62263cfa40a0176399e
Moonbear <teknos@tripsit.me>
2022-05-05 09:26:16 -0500

    One more spam issue

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -25,11 +25,12 @@ module.exports = {
         const count = reaction.count;
         // logger.debug(`[${PREFIX}] guild_id: ${guild_id}`);
         // logger.debug(`[${PREFIX}] reaction.message.guild.id: ${reaction.message.guild.id}`);
+        // If we're not in the TripSit guild, don't do this.
         if (reaction.message.guild.id !== guild_id) { return; }
         logger.debug(`[${PREFIX}] ${user.username} gave ${reaction_emoji.name} to ${reaction_author.username} in ${reaction.message.guild}!`);
         const command = client.commands.get('chitragupta');
         await command.execute('chitragupta', user, 1, reaction_emoji.toString(), reaction_author);
-        if (count >= 3 && reaction_emoji.name == 'ts_down') {
+        if (count == 3 && reaction_emoji.name == 'ts_down') {
             if (reaction.message.member.isCommunicationDisabled()) { return; }
             logger.debug(`[${PREFIX}] ${user.username} has been downvoted three times, muting!`);
             // One week is the maximum time to mute
@@ -38,9 +39,8 @@ module.exports = {
             const moderator_role = reaction.message.guild.roles.cache.find(role => role.id === role_moderator_id);
             reaction.message.reply(`Hey ${moderator_role}s! ${reaction_author.username} was downvoted three times for this, please review!`);
         }
-        if (count >= 3 && reaction_emoji.name == 'ts_up') {
-            const moderator_role = reaction.message.guild.roles.cache.find(role => role.id === role_moderator_id);
-            reaction.message.channel.send(`Hey ${moderator_role}s! ${reaction_author.username} has been upvoted three times, great work!`);
+        if (count == 3 && reaction_emoji.name == 'ts_up') {
+            reaction.message.channel.send(`${reaction_author.username} has been upvoted three times, great work!`);
         }
         return;
     },
