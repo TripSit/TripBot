9f86fc5360a4de9900118e737afb28cb263d4fdf
SevenCatsHere <sevencats@tripsit.me>
2022-05-06 10:44:03 -0400

    remove psychedelic calculator

--- src/commands/guild/button.js
+++ src/commands/guild/button.js
@@ -21,14 +21,16 @@ module.exports = {
     const channelSanctuary = interaction.client.channels.cache.get(channelSanctuaryId);
     const channelGeneral = interaction.client.channels.cache.get(channelGeneralId);
 
-    const buttonText = stripIndents`Welcome to the TripSit room!\n
-        Questions on drugs? Make a thread in ${channelQuestions}!\n
-        Don't need immediate help but want a peaceful chat? Come to ${channelSanctuary}!\n
-        **Under the influence of something and need help? Click the buttom below!**
-        This will create a new thread and alert the community that you need assistance!
-        🛑 Please do not message helpers or tripsitters directly! 🛑\n
-        All other topics of conversation are welcome in ${channelGeneral}!\n
-        Stay safe!`;
+    const buttonText = stripIndents`
+      Welcome to the TripSit room!\n
+      Questions on drugs? Make a thread in ${channelQuestions}!\n
+      Don't need immediate help but want a peaceful chat? Come to ${channelSanctuary}!\n
+      **Under the influence of something and need help? Click the buttom below!**
+      This will create a new thread and alert the community that you need assistance!
+      🛑 Please do not message helpers or tripsitters directly! 🛑\n
+      All other topics of conversation are welcome in ${channelGeneral}!\n
+      Stay safe!
+    `;
 
     // Create a new button embed
     const row = new MessageActionRow()
