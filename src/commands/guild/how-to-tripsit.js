ca19d5ec3536e652ba6494afd35a7a345b662304
Moonbear <teknos@tripsit.me>
2022-05-19 09:00:58 -0500

    Re-doing the entire tripsit experience with modals

--- src/commands/guild/how-to-tripsit.js
+++ src/commands/guild/how-to-tripsit.js
@@ -9,17 +9,20 @@ const template = require('../../utils/embed-template');
 
 const PREFIX = path.parse(__filename).name;
 
-const tripsitButton = new MessageAttachment('./src/assets/img/tripsitButton.png');
-const firstResponse = new MessageAttachment('./src/assets/img/firstResponse.png');
-const privateThread = new MessageAttachment('./src/assets/img/privateThread.png');
-const privateMessage = new MessageAttachment('./src/assets/img/privateMessage.png');
-const discussThread = new MessageAttachment('./src/assets/img/discussThread.png');
-const discussMessage = new MessageAttachment('./src/assets/img/discussMessage.png');
+const tripsitButton = new MessageAttachment('./src/assets/img/1_button.png');
+const userInput = new MessageAttachment('./src/assets/img/2_input.png');
+const firstResponse = new MessageAttachment('./src/assets/img/3_firstResponse.png');
+const privateThread = new MessageAttachment('./src/assets/img/4_privateThread.png');
+const privateMessage = new MessageAttachment('./src/assets/img/5_privateMessage.png');
+const discussThread = new MessageAttachment('./src/assets/img/6_discussionThread.png');
+const discussMessage = new MessageAttachment('./src/assets/img/7_discussionMessage.png');
 
 const channelTripsitId = process.env.channel_tripsit;
+const channelTripsittersId = process.env.channel_tripsitters;
+
 // const channelSanctuaryId = process.env.channel_sanctuary;
 // const channelDrugQuestionsId = process.env.channel_drugquestions;
-const channelTripsittersId = process.env.channel_tripsitters;
+const channelTripsitInfoId = process.env.channel_tripsitinfo;
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -37,85 +40,111 @@ module.exports = {
       .setColor('BLUE');
 
     const channelTripsit = interaction.client.channels.cache.get(channelTripsitId);
+    const channelTripsitInfo = interaction.client.channels.cache.get(channelTripsitInfoId);
     const channelTripsitters = interaction.client.channels.cache.get(channelTripsittersId);
 
-    await channelTripsitters.send(stripIndents`
+    await channelTripsitInfo.send(stripIndents`
     > **About TripSit Discord**
     > TripSit has always been a bit...different.
     > Our discord is no exception: Even if you've been using discord for years please take a moment to read the info here.
     > The information on this page can help you understand some of the intricaces of this guild, for example:
     > This channel is only visible to VIP users, which means you have talked enough to reach level 2 with the Mee6 bot.
-    > **If a user has three :ts_downvote: put on a message they are put in "timeout" where they cannot speak until a moderator reviews!**
-    ᲼᲼
+    > **If a user has three <:ts_down:960161563849932892> put on a message they are put in "timeout" where they cannot speak until a moderator reviews!**
+    <:invisible:976824380564852768>
     > **How to use TripSit**
     > ${channelTripsit.toString()} is our main help channel, this is where people who need assistance will be gently pushed.
     > This room is in "thread-only" or "forum" mode so that people will see the notice posted:
-    ᲼᲼
+    <:invisible:976824380564852768>
     `);
 
-    embed.setImage('attachment://tripsitButton.png');
-    await channelTripsitters.send({ embeds: [embed], files: [tripsitButton], ephemeral: false });
+    embed.setImage('attachment://1_button.png');
+    await channelTripsitInfo.send({ embeds: [embed], files: [tripsitButton], ephemeral: false });
 
-    await channelTripsitters.send(stripIndents`
-    ᲼᲼
-    > Clicking this button will display an embed message thanking you for asking for help:
-    ᲼᲼
+    await channelTripsitInfo.send(stripIndents`
+    <:invisible:976824380564852768>
+    > Clicking this button will display a modal popup asking for information:
+    <:invisible:976824380564852768>
     `);
 
-    embed.setImage('attachment://firstResponse.png');
-    await channelTripsitters.send({ embeds: [embed], files: [firstResponse], ephemeral: false });
+    embed.setImage('attachment://2_input.png');
+    await channelTripsitInfo.send({ embeds: [embed], files: [userInput], ephemeral: false });
 
-    await channelTripsitters.send(stripIndents`
-    ᲼᲼
+    await channelTripsitInfo.send(stripIndents`
+    <:invisible:976824380564852768>
+    > All of these questions are optional but are super helpful to have handy before alerting the team.
+    > This can also potentially limit people from “testing” the button if they know this will actually submit something.
+    > When ready, the user submits the modal, and an embed message appears thanking you for asking for help:
+    <:invisible:976824380564852768>
+    `);
+
+    embed.setImage('attachment://3_firstResponse.png');
+    await channelTripsitInfo.send({ embeds: [embed], files: [firstResponse], ephemeral: false });
+
+    await channelTripsitInfo.send(stripIndents`
+    <:invisible:976824380564852768>
     > It will then create a new thread in the ${channelTripsit.toString()} room asking the user to chat in that thread:
-    ᲼᲼
+    <:invisible:976824380564852768>
     `);
 
-    embed.setImage('attachment://privateThread.png');
-    await channelTripsitters.send({ embeds: [embed], files: [privateThread], ephemeral: false });
+    embed.setImage('attachment://4_privateThread.png');
+    await channelTripsitInfo.send({ embeds: [embed], files: [privateThread], ephemeral: false });
 
-    await channelTripsitters.send(stripIndents`
-    ᲼᲼
+    await channelTripsitInfo.send(stripIndents`
+    <:invisible:976824380564852768>
     > When they click into this thread they will see:
-    ᲼᲼
+    <:invisible:976824380564852768>
     `);
 
-    embed.setImage('attachment://privateMessage.png');
-    await channelTripsitters.send({ embeds: [embed], files: [privateMessage], ephemeral: false });
+    embed.setImage('attachment://5_privateMessage.png');
+    await channelTripsitInfo.send({ embeds: [embed], files: [privateMessage], ephemeral: false });
 
-    await channelTripsitters.send(stripIndents`
-    ᲼᲼
+    await channelTripsitInfo.send(stripIndents`
+    <:invisible:976824380564852768>
     > This is a private thread: only people who are mentioned can see and participate.
     > By default these are the **Tripsitter** and **Helper** roles
-    ᲼᲼
+    > This thread will also auto-archive after 24 hours. This allows the user to follow-up the next day.
+    > After 7 days we manually delete the thread to preserve privacy and clean up space.
+    <:invisible:976824380564852768>
     > Meanwhile, ${channelTripsitters.toString()} is the "meta-tripsit" room to talk about encounters in ${channelTripsit.toString()}
     > When someone clicks the “I need assistance” button in the ${channelTripsit.toString()} room, a new private thread is started in ${channelTripsitters.toString()}.
     > The bot pings **Tripsitters** and **Helpers** so that these are the only people who can see this thread!
-    ᲼᲼
+    <:invisible:976824380564852768>
     `);
 
-    embed.setImage('attachment://discussThread.png');
-    await channelTripsitters.send({ embeds: [embed], files: [discussThread], ephemeral: false });
+    embed.setImage('attachment://6_discussionThread.png');
+    await channelTripsitInfo.send({ embeds: [embed], files: [discussThread], ephemeral: false });
 
-    await channelTripsitters.send(stripIndents`
-    ᲼᲼
+    await channelTripsitInfo.send(stripIndents`
+    <:invisible:976824380564852768>
     > When they click into this thread they will see the following.
     > Notice how since "moonbear" isnt @ mentioned, they wont see this thread!
-    ᲼᲼
+    <:invisible:976824380564852768>
     `);
 
-    embed.setImage('attachment://discussMessage.png');
-    await channelTripsitters.send({ embeds: [embed], files: [discussMessage], ephemeral: false });
+    embed.setImage('attachment://7_discussionMessage.png');
+    await channelTripsitInfo.send({ embeds: [embed], files: [discussMessage], ephemeral: false });
 
-    // await channelTripsitters.send(stripIndents`
-    // ᲼᲼
+    await channelTripsitInfo.send(stripIndents`
+    <:invisible:976824380564852768>
+    > In conclusion: People who need help can click the "I need assistance" button in the ${channelTripsit.toString()} room.
+    > They enter their question and a private thread is created in ${channelTripsit.toString()}.
+    > This is a self-contained channel so that trolls/randoms cannot access what they're saying.
+    > The thread created in ${channelTripsitters.toString()} is also private from non-helpers.
+    > This thread lets us coordinate tactics on the encounter without disturbing the user.
+    > When it's over the tread is archived, then deleted, but the user still has a chance to follow up!
+    > This is basically the ideal way to TripSit someone!
+    <:invisible:976824380564852768>
+    `);
+
+    // await channelTripsitInfo.send(stripIndents`
+    // <:invisible:976824380564852768>
     // > **Are you interested in helping out in the Harm Reduction Centre?**
     // > By reacting to this message you will be given the **Helper** role.
     // > This will give you access to the ${channelTripsitters.toString()} room, and \
     // you will be notified when people need assistance in the ${channelTripsit.toString()} room.
     // > You are not required to respond to notices, **this role is completely optional**, //
     // but we appreciate the help!
-    // ᲼᲼
+    // <:invisible:976824380564852768>
     // Thanks for reading, stay safe!
     // `);
 
--- src/utils/tripsitme.js
+++ src/utils/tripsitme.js
@@ -1,104 +1,28 @@
 'use strict';
 
 const path = require('path');
-const { stripIndents } = require('common-tags');
-const logger = require('./logger');
-const template = require('./embed-template');
-
 const {
-  NODE_ENV,
-  channel_tripsitters: channelTripsitters,
-  role_needshelp: roleNeedsHelp,
-  role_tripsitter: roleTripsitter,
-  role_helper: roleHelper,
-} = process.env;
+  MessageActionRow, Modal, TextInputComponent,
+} = require('discord.js');
+const logger = require('./logger');
 
 const PREFIX = path.parse(__filename).name;
 
 module.exports = {
   async execute(interaction) {
-    const patient = interaction.member;
-    const test = patient.id === process.env.ownerId || patient.id.toString() === '332687787172167680';
-
-    // Get a list of the patient's roles
-    const patientRoles = patient.roles.cache;
-    const patientRoleNames = patientRoles.map(role => role.name);
-    logger.debug(`[${PREFIX}] userRoles:`, patientRoleNames);
-
-    const needsHelpRole = interaction.guild.roles.cache.find(role => role.id === roleNeedsHelp);
-    const tripsitterRole = interaction.guild.roles.cache.find(role => role.id === roleTripsitter);
-    const helperRole = interaction.guild.roles.cache.find(role => role.id === roleHelper);
-
-    // Loop through userRoles and check if the patient has the needsHelp role
-    const hasNeedsHelpRole = patientRoleNames.some(role => role === needsHelpRole.name);
-    const hasHelperRole = patientRoleNames.some(role => role === helperRole.name);
-    logger.debug(`[${PREFIX}] hasNeedsHelpRole:`, hasNeedsHelpRole);
-
-    const patientId = patient.id.toString();
-    logger.debug(`[${PREFIX}] patientid:`, patientId);
-
-    if (hasNeedsHelpRole) {
-      const embed = template.embedTemplate()
-        .setColor('DARK_BLUE')
-        .setDescription(stripIndents`
-          Hey ${interaction.member}, you're already being helped!\n\n
-          Check your channel list for '${patient.user.username} chat here!'
-        `);
-      logger.debug(`[${PREFIX}] Done!`);
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    }
-
-    patient.roles.add(needsHelpRole);
-
-    if (hasHelperRole) {
-      patient.roles.remove(helperRole);
-    }
-
-    const privMsg = stripIndents`
-      Hey ${patient}, thank you for asking for assistance!
-      **Start off by telling us what's going on: what did you take, how much, what time?**
-      A ${test ? 'tripsitter' : tripsitterRole}s or ${test ? 'helper' : helperRole}s will be with you as soon as they're available!
-      If this is a medical emergency please contact your local /EMS: we do not call EMS on behalf of anyone.
-    `;
-
-    // Create a new thread in the interaction.channel with the
-    // patient's name and the priv_message as the startMessage
-    const thread = await interaction.channel.threads.create({
-      name: `${patient.user.username} chat here!`,
-      autoArchiveDuration: 60,
-      type: NODE_ENV === 'production' ? 'GUILD_PRIVATE_THREAD' : 'GUILD_PUBLIC_THREAD',
-      reason: `${patient.user.username} requested help`,
-    });
-
-    // send a message to the thread
-    await thread.send(privMsg);
-
-    const msg = `Hey ${patient}, thank you for asking for assistance!\n\n\
-    Click here to be taken to your private room: ${thread.toString()}\n
-    You can also click in your channel list to see your private room!`;
-    const embed = template.embedTemplate()
-      .setColor('DARK_BLUE')
-      .setDescription(msg);
-    logger.debug(`[${PREFIX}] Done!`);
-    interaction.reply({ embeds: [embed], ephemeral: true });
-
-    // Get the tripsitters channel from the guild
-    const tripsittersChannel = interaction.guild.channels.cache
-      .find(chan => chan.id === channelTripsitters);
-
-    // Create a new thread in the interaction.channel with the
-    // patient's name and the priv_message as the startMessage
-    const helperThread = await tripsittersChannel.threads.create({
-      name: `${patient.user.username} discuss here!`,
-      autoArchiveDuration: 60,
-      type: 'GUILD_PUBLIC_THREAD',
-      reason: `${patient.user.username} requested help`,
-    });
-
-    const helperMsg = stripIndents`Hey ${test ? 'tripsitter' : tripsitterRole}s and ${test ? 'helper' : helperRole}s, ${patient.user.username} can use some help, use this thread to talk about it!`;
-
-    // send a message to the thread
-    await helperThread.send(helperMsg);
+    // Create the modal
+    const modal = new Modal()
+      .setCustomId('tripsitModal')
+      .setTitle('TripSit Help Request');
+    modal.addComponents(new MessageActionRow().addComponents(new TextInputComponent()
+      .setCustomId('triageInput')
+      .setLabel('What substance? How much taken? What time?')
+      .setStyle('SHORT')));
+    modal.addComponents(new MessageActionRow().addComponents(new TextInputComponent()
+      .setCustomId('introInput')
+      .setLabel('What\'s going on? Give us the details!')
+      .setStyle('PARAGRAPH')));
+    await interaction.showModal(modal);
     logger.debug(`[${PREFIX}] finished!`);
   },
 };
