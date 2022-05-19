c58d0a6197786d1d83ac0794209b2e8db43263fd
Moonbear <teknos@tripsit.me>
2022-05-19 10:03:32 -0500

    Minor update to EMS

--- src/commands/global/ems.js
+++ src/commands/global/ems.js
@@ -18,7 +18,10 @@ module.exports = {
       .addFields(
         { name: 'Poison Control (USA)', value: 'Website: https://www.poison.org/\nPhone: (800) 222-1222\nWebhelp: https://triage.webpoisoncontrol.org/', inline: false },
         { name: 'Never Use Alone (USA)', value: 'Website: https://neverusealone.com/\nPhone: (800) 484-3731', inline: false },
-        { name: 'NORS (Canada)', value: 'Website: https://www.nors.ca/\nPhone: 1 (888) 688-6677', inline: false },
+        { name: 'National Overdose Response Service (Canada)', value: 'Website: https://www.nors.ca/\nPhone: 1 (888) 688-6677', inline: false },
+        { name: 'Talktofrank (UK)', value: 'Website: https://www.talktofrank.com/\nPhone: 0300 123 6600\nWebhelp: https://www.talktofrank.com/livechat', inline: false },
+        { name: 'NHS emergency line (UK)', value: 'Website: https://www.nhs.uk/live-well/addiction-support\nPhone: 999', inline: false },
+        { name: 'Mindzone (EU/germany)', value: 'Website: https://mindzone.info/gesundheit/drogennotfall/\nPhone: 112 (works EU wide)', inline: false },
       );
 
     if (!interaction.replied) {
