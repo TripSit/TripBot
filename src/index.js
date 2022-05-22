3c1e8a02cb2180693ef6948d903ee340bac4e0e2
Moonbear <teknos@tripsit.me>
2022-05-22 16:35:04 -0500

    Adding /vip-welcome

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ const path = require('path');
 const { Client, Intents } = require('discord.js');
 const { initializeApp, cert } = require('firebase-admin/app'); // eslint-disable-line
 const { getFirestore } = require('firebase-admin/firestore'); // eslint-disable-line
+const discordIRC = require('discord-irc').default;
 const logger = require('./utils/logger');
 const registerCommands = require('./commands');
 const registerEvents = require('./events');
 const serviceAccount = require('./assets/firebase_creds.json');
+const ircConfig = require('./assets/irc_config.json');
 
 const {
   discordToken,
-  // ircServer,
-  // ircUsername,
-  // ircPassword,
+  channelModeratorsId,
+  channelGeneralId,
+  ircServer,
+  ircUsername,
+  ircPassword,
   firebasePrivateKeyId,
   firebasePrivateKey,
   firebaseClientId,
@@ -32,12 +36,16 @@ serviceAccount.client_id = firebaseClientId;
 // logger.debug(`[${PREFIX}] client_id:       ${serviceAccount.client_id}`);
 
 // IRC Connection, this takes a while so do it first
-// irc_config[0].discordToken = discordToken;
-// irc_config[0].server = ircServer;
-// irc_config[0].ircOptions.username = ircUsername;
-// irc_config[0].ircOptions.password = ircPassword;
-// irc_config[0].webhooks['960606558549594162'] = process.env['960606558549594162'];
-// discordIRC(irc_config);
+ircConfig[0].discordToken = discordToken;
+ircConfig[0].server = ircServer;
+ircConfig[0].ircOptions.username = ircUsername;
+ircConfig[0].ircOptions.password = ircPassword;
+ircConfig[0].channelMapping = {
+  [channelModeratorsId]: '#moderators',
+  [channelGeneralId]: '#sandbox',
+};
+ircConfig[0].webhooks['960606558549594162'] = process.env['960606558549594162'];
+// discordIRC(ircConfig);
 
 // Initialize firebase app
 if (serviceAccount.private_key_id) {
