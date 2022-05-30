281a8bc4f2f9c670c1cba8cd8125c9237498e59e
Moonbear <teknos@tripsit.me>
2022-05-30 10:36:53 -0500

    Minor bug fixing

--- src/commands/guild/karma.js
+++ src/commands/guild/karma.js
@@ -32,8 +32,8 @@ module.exports = {
     const [actorData] = await getUserInfo(actor);
 
     // Transform actor data
-    const karmaReceived = actorData.discord.karma_received;
     let karmaReceivedString = '';
+    const karmaReceived = actorData.discord.karma_received;
     if (karmaReceived) {
       logger.debug(`[${PREFIX}] karma_received: ${JSON.stringify(karmaReceived, null, 2)}`);
       if (all) {
@@ -49,15 +49,24 @@ module.exports = {
     } else {
       karmaReceivedString = 'Nothing, they are a blank canvas to be discovered!';
     }
-    const karmaGiven = actorData.discord.karma_received;
-    const karmaGivenString = !karmaGiven
-      ? 'Nothing, they are a wet paintbrush ready to make their mark!'
-      : Object.entries(karmaGiven)
-        .sort((a, b) => b[1] - a[1])
-        .filter(([k]) => all
-          || ['<:ts_voteup:958721361587630210>', '<:ts_votedown:960161563849932892>'].includes(k))
-        .map(([k, v]) => `${k} — ${v}`)
-        .join('\n');
+
+    let karmaGivenString = '';
+    const karmaGiven = actorData.discord.karma_given;
+    if (karmaGiven) {
+      logger.debug(`[${PREFIX}] karmaGiven: ${JSON.stringify(karmaGiven, null, 2)}`);
+      if (all) {
+        // sort karmaGiven by value and then turn it into a string
+        const karmaGivenSorted = Object.entries(karmaGiven).sort((a, b) => b[1] - a[1]);
+        karmaGivenString = karmaGivenSorted.map(([key, value]) => `${key} — ${value}`).join('\n');
+      } else {
+        // Find 'ts_voteup' and 'ts_votedown' in the keys and then turn it into a string
+        const karmaGivenSorted = Object.entries(karmaGiven).sort((a, b) => b[1] - a[1]);
+        const karmaGivenFiltered = karmaGivenSorted.filter(([key]) => key === '<:ts_voteup:958721361587630210>' || key === '<:ts_votedown:960161563849932892>');
+        karmaGivenString = karmaGivenFiltered.map(([key, value]) => `${key} — ${value}`).join('\n');
+      }
+    } else {
+      karmaGivenString = 'Nothing, they are a blank canvas to be discovered!';
+    }
 
     const book = [];
     const randomQuoteA = karmaQuotes[Math.floor(
