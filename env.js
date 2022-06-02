dabf3d4df343857c105e049d6c691610f859e0cb
Moonbear <teknos@tripsit.me>
2022-06-02 08:24:14 -0500

    Updating ENV Example

--- env.js
+++ env.js
@@ -113,11 +113,8 @@ exports.roleStimmingId = process.env.roleStimmingId;
 exports.roleNoddingId = process.env.roleNoddingId;
 exports.roleSoberId = process.env.roleSoberId;
 
-exports.roleIrcVerifiedId = process.env.roleIrcVerifiedId;
 exports.roleTreeId = process.env.roleTreeId;
 exports.roleSproutId = process.env.roleSproutId;
 exports.roleSeedlingId = process.env.roleSeedlingId;
 exports.roleBoosterId = process.env.roleBoosterId;
-
-exports.channelModeratorsWebhook = process.env.channelModeratorsWebhook;
-exports.channelSandboxWebhook = process.env.channelSandboxWebhook;
+exports.roleIrcVerifiedId = process.env.roleIrcVerifiedId;
