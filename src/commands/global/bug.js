d0917874d0fe27de32e4c7d734470e8242195fea
Moonbear <teknos@tripsit.me>
2022-06-14 14:22:51 -0500

    Part of the button.js cleanup that i forgot to include

--- src/commands/global/bug.js
+++ src/commands/global/bug.js
@@ -4,6 +4,13 @@ const path = require('path');
 const { MessageActionRow, Modal, TextInputComponent } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const logger = require('../../utils/logger');
+const template = require('../../utils/embed-template');
+
+const {
+  discordOwnerId,
+  roleDeveloperId,
+  channelTripbotId,
+} = require('../../../env');
 
 const PREFIX = path.parse(__filename).name;
 
@@ -27,5 +34,41 @@ module.exports = {
     // Show the modal to the user
     await interaction.showModal(modal);
     logger.debug(`[${PREFIX}] finished!`);
+  },
+  async submit(interaction) {
+    const username = `${interaction.user.username}#${interaction.user.discriminator}`;
+    const guildMessage = `${interaction.guild.name ? ` in ${interaction.guild.name}` : 'DM'}`;
+
+    const bugReport = interaction.fields.getTextInputValue('bugReport');
+    logger.debug(`[${PREFIX}] bugReport:`, bugReport);
+
+    const botOwner = interaction.client.users.cache.get(discordOwnerId);
+    const botOwnerEmbed = template.embedTemplate()
+      .setColor('RANDOM')
+      .setDescription(`Hey ${botOwner.toString()},\n${username}${guildMessage} reports:\n${bugReport}`);
+    botOwner.send({ embeds: [botOwnerEmbed] });
+
+    const developerRole = interaction.guild.roles.cache.find(role => role.id === roleDeveloperId);
+    const devChan = interaction.client.channels.cache.get(channelTripbotId);
+    const devEmbed = template.embedTemplate()
+      .setColor('RANDOM')
+      .setDescription(`Hey ${developerRole.toString()}s, a user submitted a bug report:\n${bugReport}`);
+    devChan.send({ embeds: [devEmbed] });
+
+    const embed = template.embedTemplate()
+      .setColor('RANDOM')
+      .setTitle('Thank you!')
+      .setDescription('I\'ve submitted this feedback to the bot owner. \n\nYou\'re more than welcome to join the TripSit server and speak to Moonbear directly if you want! Check the /contact command for more info.');
+    if (!interaction.replied) {
+      interaction.reply({
+        embeds: [embed],
+        ephemeral: true,
+      });
+    } else {
+      interaction.followUp({
+        embeds: [embed],
+        ephemeral: false,
+      });
+    }
   },
 };
