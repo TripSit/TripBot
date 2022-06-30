6dfceec9305061506f9b47154348066ac22b9aa5
Moonbear <teknos@tripsit.me>
2022-06-30 10:35:14 -0500

    Updated experience/firebase to accept guest users

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -9,6 +9,7 @@ const { getUserInfo, setUserInfo } = require('./firebase');
 const PREFIX = path.parse(__filename).name;
 
 const {
+  discordGuildId,
   channelBotspamId,
   channelTripsitId,
   channelSanctuaryId,
@@ -42,10 +43,8 @@ const tripsitterChannels = [
 
 module.exports = {
   async experience(message) {
-    logger.debug(`[${PREFIX}] Message sent by ${message.author.username} in ${message.channel.name} on ${message.guild}`);
-
-    // If the user is not a member of the guild, ignore it
-    if (!message.member) { return; }
+    // If not in tripsit, ignore it
+    if (message.guildId !== discordGuildId) { return; }
 
     // Check if the user who sent this message is a guild user
     if (message.member) {
@@ -53,6 +52,72 @@ module.exports = {
         logger.debug(`[${PREFIX}] Message sent by a user with an ignored role`);
         return;
       }
+    }
+
+    logger.debug(`[${PREFIX}] Message sent by ${message.author.username} in ${message.channel.name} on ${message.guild}`);
+
+    let actor = message.author;
+
+    // let targetFromIrc = null;
+    // let targetFromDiscord = null;
+    // let targetIsMember = null;
+
+    // logger.debug(`[${PREFIX}] message.member: ${JSON.stringify(message.member, null, 2)}`);
+    // If the user is not a member of the guild, then this probably came from IRC
+    if (!message.member) {
+      // Check if this message came from a tripsitting channel
+      // If so, ignore it, cuz they're likely asking for help
+      if (tripsitterChannels.includes(message.channel.id)
+      || tripsitterChannels.includes(message.channel.parentId)) {
+        logger.debug(`[${PREFIX}] Message sent in a tripsitter channel from IRC`);
+        return;
+      }
+
+      // Do a whois on the user to get their account name
+      let data = null;
+      await global.ircClient.whois(message.author.username, async resp => {
+        data = resp;
+      });
+
+      // This is a hack substanc3 helped create to get around the fact that the whois command
+      // is asyncronous by default, so we need to make this syncronous
+      while (data === null) {
+          await new Promise(resolve => setTimeout(resolve, 100)); // eslint-disable-line
+      }
+
+      // logger.debug(`[${PREFIX}] data ${JSON.stringify(data, null, 2)}`);
+      // {
+      //   "nick": "Teknos",
+      //   "user": "~teknos",
+      //   "host": "tripsit/founder/Teknos",
+      //   "realname": "Eric",
+      //   "channels": [
+      //   ],
+      //   "server": "innsbruck.tripsit.me",
+      //   "serverinfo": "TripSit IRC Private Jet Receipt Server",
+      //   "operator": "Cantillating grace and they can't keep my pace",
+      //   "idle": "0",
+      //   "account": "Teknos",
+      //   "accountinfo": "is logged in as"
+      // }
+
+      // Check if the user is FOUND on IRC, if not, ignore it
+      if (!data.host) {
+        logger.debug(`[${PREFIX}] ${message.author.username} not found on IRC, ignoring!`);
+        return;
+      }
+
+      // Check if the user is REGISTERED on IRC, if not, ignore it
+      // Disabling this because we need to know when the people last asking for help last talked
+      // if (!data.account) {
+      //   logger.debug(`[${PREFIX}] ${message.author.username} is not registered on IRC!`);
+      //   return;
+      // }
+
+      // targetFromIrc = true;
+      // targetFromDiscord = false;
+      // targetIsMember = false;
+      actor = data;
     }
 
     let messageChannelId = message.channel.id;
@@ -67,10 +132,9 @@ module.exports = {
       // logger.debug(`[${PREFIX}] Message sent in a non-tripsitter channel`);
       expType = 'general';
     }
-    // logger.debug(`[${PREFIX}] Experience type: ${expType}`);
+    logger.debug(`[${PREFIX}] Experience type: ${expType}`);
 
     // Get user data
-    const actor = message.author;
     const [actorData, actorFbid] = await getUserInfo(actor);
 
     // Get random value between 15 and 25
@@ -101,7 +165,7 @@ module.exports = {
           let level = experienceData.level;
           const expToLevel = 5 * (level ** 2) + (50 * level) + 100;
 
-          logger.debug(`[${PREFIX}] ${actor.username} (lv${level}) +${expPoints} ${expType} exp | TotalExp: ${totalExpPoints}, LevelExp: ${levelExpPoints}, ExpToLevel ${level + 1}: ${expToLevel}`);
+          logger.debug(`[${PREFIX}] ${actor.username ? actor.username : actor.nick} (lv${level}) +${expPoints} ${expType} exp | TotalExp: ${totalExpPoints}, LevelExp: ${levelExpPoints}, ExpToLevel ${level + 1}: ${expToLevel}`);
           if (expToLevel < levelExpPoints) {
             logger.debug(`[${PREFIX}] ${actor.username} has leveled up to ${expType} level ${level + 1}!`);
 
