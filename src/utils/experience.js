ff3272cfcb78be5532ca6dbc7a3392a1b3d77426
Moonbear <teknos@tripsit.me>
2022-07-03 19:18:11 -0500

    Update experience.js

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -128,7 +128,8 @@ module.exports = {
         return;
       }
 
-      logger.debug(`[${PREFIX}] ${message.nick} (${message.host.split('/')[1]}) said ${message.args[1]} in ${message.args[0]}`);
+      const memberHost = message.host.split('/')[1];
+      logger.debug(`[${PREFIX}] ${message.nick}${memberHost ? ` (${memberHost}) ` : ' '}said ${message.args[1]} in ${message.args[0]}`);
       actor = message;
 
       // Determine what kind of experience to give
