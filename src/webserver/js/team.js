ec7e7d4edb6c8082b08bf87c608aeb57abbe9973
Moonbear <teknos@tripsit.me>
2022-07-12 07:54:35 -0500

    Current changes

--- src/webserver/js/team.js
+++ src/webserver/js/team.js
@@ -31,7 +31,7 @@ for (let indexOne = 0; indexOne < owners.length; indexOne++) {
         }, 1000);
       }
 
-      const ownerList = `<div id='trigger' class='card' style='margin: 15px;'><div class='banner'><img src='${output.url}'></div></br></br></br></br><h2 class='name'>${output.username}</h2><div class='title'><h1 id='trigger2' style='font-size: 26px; color: #000000;'>${elementOwners.post}</h2></div><div class='actions'><div class='follow-btn' style='margin-bottom: 5px;'><a href='${elementOwners.GHURL}' target='_blank'><button style='color: #000000;'>GitHub</button></a></div><div class='follow-btn'><a href='${elementOwners.YTURL}' target='_blank'><button style='color: #000000;'>YouTube</button></a></div></div></div>`;
+      const ownerList = `<div id='trigger' class='card' style='margin: 15px;'><div class='banner'><img src='${output.url}'></div></br></br></br></br><h2 class='name'>${output.username}</h2><div class='title'><h1 id='trigger2' style='font-size: 26px; color: #000000;'>${elementOwners.post}</h2></div><div class='actions'><div class='follow-btn' style='margin-bottom: 5px;'><a href='${elementOwners.GHURL}' target='_blank'><button style='color: #000000;'>GitHub</button></a></div><div class='follow-btn'></div></div></div>`;
       boxOwners.innerHTML += ownerList;
     });
 }
