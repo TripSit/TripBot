45e0f430d2610f575787f07ca25569a540ecdefd
Moonbear <teknos@tripsit.me>
2022-07-20 16:39:17 -0500

    Created unit conversion (every unit)

--- src/discord/commands/global/convert.js
+++ src/discord/commands/global/convert.js
@@ -2,13 +2,9 @@
 
 const path = require('path');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const configureMeasurements = require('convert-units');
-const { allMeasures } = require('convert-units');
-const calcBenzo = require('../../../global/utils/calc-benzo');
+const convert = require('convert-units');
 const logger = require('../../../global/utils/logger');
 const template = require('../../utils/embed-template');
-
-const convert = configureMeasurements(allMeasures);
 
 const PREFIX = path.parse(__filename).name;
 
@@ -16,28 +12,35 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('convert')
     .setDescription('Convert one unit into another')
-    .addIntegerOption(option => option.setName('value')
+    .addStringOption(option => option.setName('value')
       .setDescription('#')
       .setRequired(true))
     .addStringOption(option => option.setName('units')
       .setDescription('What unit?')
       .setRequired(true)
       .setAutocomplete(true))
-    .addStringOption(option => option.setName('intoUnits')
+    .addStringOption(option => option.setName('into_units')
       .setDescription('Convert into?')
       .setRequired(true)
       .setAutocomplete(true)),
 
   async execute(interaction) {
-    const value = interaction.options.getInteger('value');
+    const value = parseFloat(interaction.options.getString('value'));
     const units = interaction.options.getString('units');
-    const intoUnits = interaction.options.getString('intoUnits');
+    const intoUnits = interaction.options.getString('into_units');
 
+    if (Number.isNaN(value)) {
+      if (interaction.replied) interaction.followUp('You must enter a number.');
+      else interaction.reply('You must enter a number.');
+      return;
+    }
+
+    logger.debug(`${PREFIX}: ${value} ${units} into ${intoUnits}`);
     const result = convert(value).from(units).to(intoUnits);
 
     const embed = template.embedTemplate()
-      .setTitle(`${value} ${units} is ${result} ${intoUnits}`)
-      .setDescription(`${value} ${units} is ${result} ${intoUnits}`);
+      .setTitle(`${value} ${units} is ${result} ${intoUnits}`);
+      // .setDescription(`${value} ${units} is ${result} ${intoUnits}`);
     if (interaction.replied) interaction.followUp({ embeds: [embed] });
     else interaction.reply({ embeds: [embed] });
     logger.debug(`[${PREFIX}] finished!`);
