353c1c43b1fe89392c33030019c2d91f50b711f8
Moonbear <teknos@tripsit.me>
2022-07-21 14:21:11 -0500

    Updating imgur cuz im easilly distracted

--- src/discord/commands/guild/d.imgur.js
+++ src/discord/commands/guild/d.imgur.js
@@ -11,26 +11,41 @@ module.exports = {
     .setName('imgur')
     .setDescription('Search Imgur')
     .addStringOption(option => option
-      .setName('query')
+      .setName('search')
       .setDescription('What are you looking for?')
-      .setRequired(true)),
-
+      .setRequired(true))
+    .addStringOption(option => option
+      .setName('sort')
+      .setDescription('How should the results be sorted?')
+      .addChoice('Default: Top', 'top')
+      .addChoice('Viral', 'viral')
+      .addChoice('Time', 'time'))
+    .addStringOption(option => option
+      .setName('window')
+      .setDescription('How far back should we look?')
+      .addChoice('Default: All', 'all')
+      .addChoice('Day', 'day')
+      .addChoice('Week', 'week')
+      .addChoice('Month', 'month')
+      .addChoice('Year', 'year')),
   async execute(interaction) {
-    const query = interaction.options.getString('query');
+    // Sometimes the API takes a few seconds to respond.
+    await interaction.reply('Searching Imgur...');
+    const search = interaction.options.getString('search');
+    const sort = interaction.options.getString('sort') || 'top';
+    const window = interaction.options.getString('window') || 'all';
+    logger.debug(`[${PREFIX}] query: ${search}`);
+    logger.debug(`[${PREFIX}] sort: ${sort}`);
+    logger.debug(`[${PREFIX}] window: ${window}`);
+
+    const query = `https://api.imgur.com/3/gallery/search/${sort !== null ? `${sort}/` : ''}${window !== null ? `${window}/` : ''}?q=${search}`;
+    logger.debug(`[${PREFIX}] query: ${query}`);
 
     const url = await imgur.search(query);
 
     logger.debug(`[${PREFIX}] url: ${url}`);
 
     if (!interaction.replied) interaction.reply(url);
-    else interaction.followUp(url);
-
-    // const embed = template.embedTemplate()
-    //   .setTitle(`Imgur search: ${query}`)
-    //   .setImage(await url)
-    //   .setURL(await url)
-    //   .setColor(0x89c623);
-
-    // interaction.reply({ embeds: [embed], ephemeral: false });
+    else interaction.editReply(url);
   },
 };
--- src/global/utils/imgur.js
+++ src/global/utils/imgur.js
@@ -11,11 +11,8 @@ const {
 module.exports = {
 
   async search(query) {
-    const url = `https://api.imgur.com/3/gallery/search/?q=${query}`;
-    logger.debug(`[${PREFIX}] url: ${url}`);
-    logger.debug(`[${PREFIX}] imgurSecret: ${imgurId}`);
     return new Promise((resolve, reject) => {
-      axios.get(url, {
+      axios.get(query, {
         headers: {
           Authorization: `Client-ID ${imgurId}`,
         },
@@ -23,36 +20,19 @@ module.exports = {
         let imageLink = '';
 
         if (res.data.data.length > 0) {
-          let topUps = 0;
-          let topPost = {};
-
-          // Get the most popular result
-          // eslint-disable-next-line no-restricted-syntax
-          for (const i in res.data.data) {
-            if (res.data.data[i].ups) {
-              // logger.debug(`[${PREFIX}] ups: ${res.data.data[i].ups}`);
-              if (res.data.data[i].ups > topUps) {
-                topPost = res.data.data[i];
-                topUps = res.data.data[i].ups;
-                // logger.debug(`[${PREFIX}] New top post!`);
-              }
-            }
-          }
-          // logger.debug(`[${PREFIX}] topPost: ${JSON.stringify(topPost, null, 2)}`);
-
           // If it's an album, pull the first image
-          if (topPost.is_album) {
+          if (res.data.data[0].is_album) {
             // Only gifv and mp4s display on discord.
             // Find the "gifv" property, if that doesnt exist find the "link"
-            if (topPost.images[0].gifv) {
-              imageLink = topPost.images[0].gifv;
+            if (res.data.data[0].images[0].gifv) {
+              imageLink = res.data.data[0].images[0].gifv;
             } else {
-              imageLink = topPost.images[0].link;
+              imageLink = res.data.data[0].images[0].link;
             }
-          } else if (topPost.gifv) {
-            imageLink = topPost.gifv;
-          } else if (topPost.link) {
-            imageLink = topPost.link;
+          } else if (res.data.data[0].gifv) {
+            imageLink = res.data.data[0].gifv;
+          } else if (res.data.data[0].link) {
+            imageLink = res.data.data[0].link;
           }
         } else {
           logger.debug(`[${PREFIX}] No results found!`);
