c2a1101504c6da3c9d171a176e519c1a8bdd261e
Moonbear <teknos@tripsit.me>
2022-08-22 09:33:22 -0500

    Fix status loop for v14

--- src/discord/utils/status-loop.js
+++ src/discord/utils/status-loop.js
@@ -1,7 +1,12 @@
 'use strict';
 
-// const PREFIX = require('path').parse(__filename).name;
-// const logger = require("../../global/utils/logger");
+const PREFIX = require('path').parse(__filename).name;
+const { ActivityType } = require('discord.js');
+const logger = require('../../global/utils/logger');
+
+const {
+  NODE_ENV,
+} = require('../../../env');
 
 // https://discord.js.org/#/docs/discord.js/stable/typedef/ActivityType
 // https://discord.com/developers/docs/game-sdk/activities#data-models-activitytype-enum
@@ -21,21 +26,28 @@
 // 5  - COMPETING - Competing in {message}
 
 const activities = [
-  { type: 'PLAYING', message: 'with a test kit' },
-  { type: 'WATCHING', message: 'out for impure drugs' },
-  { type: 'LISTENING', message: 'to someone\'s problem' },
+  { type: ActivityType.Playing, message: 'with a scale' },
+  { type: ActivityType.Watching, message: 'test kit results' },
+  { type: ActivityType.Listening, message: 'someone talk' },
 ];
+
+const delay = NODE_ENV === 'production' ? 5 * 60 * 1000 : 5 * 1000;
 
 module.exports = {
   name: 'ready',
   once: true,
   async startStatusLoop(client) {
-    // logger.debug(`[${PREFIX}] Starting status loop...`);
+    logger.info(`[${PREFIX}] Starting status loop...`);
+
     let state = 0;
+    let presence = activities[state];
+    client.user.setActivity(presence.message, { type: presence.type });
+
     setInterval(() => {
       state = (state + 1) % activities.length;
-      const presence = activities[state];
+      presence = activities[state];
+      logger.debug(`[${PREFIX}] Setting activity to ${presence.type} ${presence.message}`);
       client.user.setActivity(presence.message, { type: presence.type });
-    }, 5 * 60 * 1000);
+    }, delay);
   },
 };
