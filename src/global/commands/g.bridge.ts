a5df71b132ea186f7b4f261fe05c28268171aedb
spr1t3-0 <ausfahrt@riseup.net>
2022-09-10 13:08:37 +0200

    Issue #250: Add to /bridge that IRC users cannot see replies

--- src/global/commands/g.bridge.ts
+++ src/global/commands/g.bridge.ts
@@ -6,5 +6,6 @@ export async function bridgeWording():Promise<any> {
   return `
   Channels with a 🔗 are 'linked' with our IRC, so messages sent in in Discord are also sent to IRC, and vis versa.
   The "bot" tag is used to identify messages from the bridge, not robots! TS is used on IRC to identify bridge messages.
+  Please note that users on IRC can't see replies from Discord.
   `;
 };
