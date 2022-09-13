1962ad44175ae00dd421ca70262035984280bce5
Moonbear <teknos@tripsit.me>
2022-09-13 08:16:04 -0500

    Update urbanDefine.ts

--- src/discord/commands/global/urbanDefine.ts
+++ src/discord/commands/global/urbanDefine.ts
@@ -37,52 +37,49 @@ export const discordTemplate: SlashCommand = {
     // Sort data by the thumbs_up value
     data.list.sort((a:any, b:any) => b.thumbs_up - a.thumbs_up);
     // logger.debug(`[${PREFIX}] data: ${JSON.stringify(data, null, 2)}`);
+    const definition = `${data.list[0].definition.length > 1024 ?
+      `${data.list[0].definition.slice(0, 1020)}...` :
+      data.list[0].definition}`.replace(/\[|\]/g, '');
+
+    const example = `${data.list[0].example}`.replace(/\[|\]/g, '');
+
+    const upvotes = `${data.list[0].thumbs_up}`;
+    const downvotes = `${data.list[0].thumbs_down}`;
+
     const embed = embedTemplate()
-        .setTitle(`Definition for: ${term}`)
-        .addFields(
-            {
-              name: `Definition A (+${data.list[0].thumbs_up}/-${data.list[0].thumbs_down})`,
-              value: `${data.list[0].definition.length > 1024 ?
-                `${data.list[0].definition.slice(0, 1020)}...` :
-                data.list[0].definition}`,
-              inline: false,
-            },
-            {
-              name: 'Example A',
-              value: data.list[0].example,
-              inline: false,
-            },
-        );
-    if (data.list[1]) {
-      embed.addFields(
-          {
-            name: `Definition B (+${data.list[1].thumbs_up}/-${data.list[1].thumbs_down})`,
-            value: `${data.list[1].definition.length > 1024 ?
-              `${data.list[1].definition.slice(0, 1020)}...` :
-              data.list[1].definition}`,
-            inline: false},
-          {
-            name: 'Example B',
-            value: data.list[1].example,
-            inline: false,
-          },
-      );
-    }
-    if (data.list[2]) {
-      embed.addFields(
-          {
-            name: `Definition C (+${data.list[2].thumbs_up}/-${data.list[2].thumbs_down})`,
-            value: `${data.list[2].definition.length > 1024 ?
-              `${data.list[2].definition.slice(0, 1020)}...` : data.list[2].definition}`,
-            inline: false,
-          },
-          {
-            name: 'Example C',
-            value: data.list[2].example,
-            inline: false,
-          },
-      );
-    }
+        .setDescription(`**Definition for *${term}* ** (+${upvotes}/-${downvotes})
+        ${definition}
+        Example: ${example}`);
+    // if (data.list[1]) {
+    //   embed.addFields(
+    //       {
+    //         name: `Definition B (+${data.list[1].thumbs_up}/-${data.list[1].thumbs_down})`,
+    //         value: `${data.list[1].definition.length > 1024 ?
+    //           `${data.list[1].definition.slice(0, 1020)}...` :
+    //           data.list[1].definition}`,
+    //         inline: false},
+    //       {
+    //         name: 'Example B',
+    //         value: data.list[1].example,
+    //         inline: false,
+    //       },
+    //   );
+    // }
+    // if (data.list[2]) {
+    //   embed.addFields(
+    //       {
+    //         name: `Definition C (+${data.list[2].thumbs_up}/-${data.list[2].thumbs_down})`,
+    //         value: `${data.list[2].definition.length > 1024 ?
+    //           `${data.list[2].definition.slice(0, 1020)}...` : data.list[2].definition}`,
+    //         inline: false,
+    //       },
+    //       {
+    //         name: 'Example C',
+    //         value: data.list[2].example,
+    //         inline: false,
+    //       },
+    //   );
+    // }
     if (interaction.replied) {
       interaction.followUp({embeds: [embed], ephemeral: false});
     } else {
