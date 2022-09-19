89bd9483598a022c10ea01f97f231162e4469f94
Moonbear <teknos@tripsit.me>
2022-09-19 10:03:15 -0500

    Add limiter to ~tripsit

--- src/discord/utils/messageCommand.ts
+++ src/discord/utils/messageCommand.ts
@@ -9,6 +9,8 @@ import * as path from 'path';
 import {stripIndents} from 'common-tags';
 const PREFIX = path.parse(__filename).name;
 
+const helpCounter = new Map<string, number>();
+
 /**
  * Template
  * @param {Message} message The message that was sent
@@ -26,10 +28,22 @@ export async function messageCommand(message: Message): Promise<void> {
     const command = message.content.split(' ')[0].slice(1);
     logger.debug(`[${PREFIX}] command: ${command}`);
     if (command === 'tripsit') {
+      const now = Date.now().valueOf();
+      if (helpCounter.has(message.author.id)) {
+        const lastTime = helpCounter.get(message.author.id);
+        if (now - lastTime! < 1000 * 60 * 5) {
+          message.channel.send(stripIndents`Hey ${message.member!.displayName}, you just used that command, \
+give people a chance to answer 😄 If no one answers in 5 minutes you can try again.`);
+          return;
+        }
+      }
       const roleTripsitter = message.guild!.roles.cache.find((role) => role.id === env.ROLE_TRIPSITTER) as Role;
       const roleHelper = message.guild!.roles.cache.find((role) => role.id === env.ROLE_HELPER) as Role;
       message.channel.send(
-          `Hey ${roleTripsitter} and ${roleHelper}, ${message.member!.displayName} could use some help!`);
+          `Hey ${message.member!.displayName}, thank you for asking for help! We've notified our ${roleTripsitter} and\
+${roleHelper}. Can you start off by telling us how much you took and the details of your problem?`);
+      // Update helpCounter with the current date that the user sent this command
+      helpCounter.set(message.author.id, Date.now().valueOf());
     } else {
       message.channel.send(`Hey ${message.member!.displayName}, use /help to get a list of commands on discord!`);
     }
