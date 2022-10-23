8ef4173ac6387cc4c4de2255edd09e0ff8079e11
Moonbear <teknos@tripsit.me>
2022-10-23 14:15:44 -0500

    Remove IRC and Telegram code

--- src/telegram/commands/t.combochart.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { Composer } = require('telegraf');
-const { combochart } = require('../../global/utils/combochart');
-
-module.exports = Composer.command('combochart', async ctx => {
-  const url = await combochart();
-  ctx.reply(url);
-});
--- src/telegram/commands/t.topic.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { Composer } = require('telegraf');
-const { topic } = require('../../global/utils/topic');
-
-module.exports = Composer.command('topic', async ctx => {
-  const data = await topic();
-  ctx.reply(data);
-});
