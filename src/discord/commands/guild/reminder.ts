5a176202873239f3f8c21011d4a24bbdfc08b1e2
Moonbear <teknos@tripsit.me>
2022-10-24 09:29:26 -0500

    Update reminder.ts

--- src/discord/commands/guild/reminder.ts
+++ src/discord/commands/guild/reminder.ts
@@ -1,16 +1,10 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 import {
-  ActionRowBuilder,
-  ModalBuilder,
-  TextInputBuilder,
-  Colors,
   SlashCommandBuilder,
   TextChannel,
+  GuildMember,
+  Colors,
 } from 'discord.js';
-import {
-  TextInputStyle,
-} from 'discord-api-types/v10';
 import {SlashCommand} from '../../@types/commandDef';
 import {embedTemplate} from '../../utils/embedTemplate';
 import env from '../../../global/utils/env.config';
@@ -19,8 +13,26 @@ import * as path from 'path';
 const PREFIX = path.parse(__filename).name;
 
 const reminderDict = {
-  [env.CHANNEL_TEAMTRIPSIT]: 'Reminder: bloop is cool!',
-  [env.CHANNEL_GENERAL]: 'Remember, #general is the first place new members see and is for SFW general discussion. We ask that you move all NSFW conversation including most drug-related talk to #lounge or the appropriate drug-specific channels to ensure a comfortable landing space for new members. On behalf of all the staff here at TripSit, we thank you for your understanding! <3',
+  [env.CHANNEL_ANNOUNCEMENTS]: [
+    `EmbedTitle`,
+    `EmbedDescription`,
+  ],
+  [env.CHANNEL_MODHAVEN]: [
+    `Keep team talk to #teamtripsit!`,
+    `While we love to see people discussing the org, we want to make sure everyone is on the same page. Please keep all team talk to #teamtripsit!`,
+  ],
+  [env.CHANNEL_TEAMTRIPSIT]: [
+    'Keep social talk to #modhaven!',
+    'While we all love to have a good time, we want to keep this channel easy to scan for people to keep up with news and updates. Please keep social talk to #modhaven!',
+  ],
+  [env.CHANNEL_GENERAL]: [
+    'Keep #general welcoming and move drug talk to #lounge',
+    '#general is the first channel new members see. To ensure we make a good impression, we ask that you move all NSFW conversation, including most drug-related talk, to #lounge or the appropriate Backstage channel to ensure a comfortable landing space for new members, thank you!',
+  ],
+  [env.CHANNEL_SANCTUARY]: [
+    'Keep #sanctuary slow and positive!',
+    '#sanctuary is a positivity-enforced channel for people currently on substances. Please keep the conversation slow and positive, and remember that we are here to help!',
+  ],
 };
 
 export const reminder: SlashCommand = {
@@ -30,10 +42,18 @@ export const reminder: SlashCommand = {
   async execute(interaction) {
     logger.debug(`[${PREFIX}] starting!`);
 
-    interaction.channel?.send(reminderDict[interaction.channelId]);
+    const reminder = embedTemplate()
+      .setColor(Colors.Red)
+      .setTitle(`REMINDER: ${reminderDict[interaction.channelId][0]}`)
+      .setDescription(reminderDict[interaction.channelId][1]);
+
+    interaction.channel?.send({embeds: [reminder]});
 
     interaction.reply({content: 'Reminder sent!', ephemeral: true});
 
-    logger.debug(`[${PREFIX}] starting!`);
+    const channelBotlog = interaction.guild!.channels.cache.get(env.CHANNEL_BOTLOG) as TextChannel;
+    if (channelBotlog) {
+      channelBotlog.send(`${(interaction.member as GuildMember).displayName} sent a reminder to ${(interaction.channel as TextChannel).name}`);
+    }
   },
 };
