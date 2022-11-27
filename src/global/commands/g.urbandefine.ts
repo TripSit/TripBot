b31b057c4b9c73e1c7501429b9f8a109e26130b3
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2022-11-27 11:15:00 -0600

    Urban define debugging

--- src/global/commands/g.urbandefine.ts
+++ src/global/commands/g.urbandefine.ts
@@ -14,7 +14,8 @@ export default urbandefine;
  * @return {string} definition
  */
 export async function urbandefine(term:string) {
-  // log.debug(`[${PREFIX}] UrbanDefine looking for ${term}`);
+  log.debug(`[${PREFIX}] UrbanDefine looking for ${term}`);
+  log.debug(`[${PREFIX}] RAPID_TOKEN: ${env.RAPID_TOKEN.slice(0, 4)}`);
   const { data } = await axios.get(
     'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
     {
@@ -27,37 +28,37 @@ export async function urbandefine(term:string) {
     },
   );
 
-    type UrbanDefinition = {
-      definition: string,
-      permalink: string,
-      thumbs_up: number,
-      sound_urls: string[],
-      author: string,
-      word: string,
-      defid: number,
-      current_vote: string,
-      written_on: string,
-      example: string,
-      thumbs_down: number
-    };
+  type UrbanDefinition = {
+    definition: string,
+    permalink: string,
+    thumbs_up: number,
+    sound_urls: string[],
+    author: string,
+    word: string,
+    defid: number,
+    current_vote: string,
+    written_on: string,
+    example: string,
+    thumbs_down: number
+  };
 
-    // log.debug(`[${PREFIX}] UrbanDefine found ${data.list.length} results`);
+  log.debug(`[${PREFIX}] UrbanDefine found ${data.list.length} results`);
 
-    // Sort data by the thumbs_up value
-    (data.list as UrbanDefinition[]).sort((a, b) => b.thumbs_up - a.thumbs_up);
-    // log.debug(`[${PREFIX}] data: ${JSON.stringify(data, null, 2)}`);
-    const definition = `${data.list[0].definition.length > 1024
-      ? `${data.list[0].definition.slice(0, 1020)}...`
-      : data.list[0].definition}`.replace(/\[|\]/g, '');
+  // Sort data by the thumbs_up value
+  (data.list as UrbanDefinition[]).sort((a, b) => b.thumbs_up - a.thumbs_up);
+  // log.debug(`[${PREFIX}] data: ${JSON.stringify(data, null, 2)}`);
+  const definition = `${data.list[0].definition.length > 1024
+    ? `${data.list[0].definition.slice(0, 1020)}...`
+    : data.list[0].definition}`.replace(/\[|\]/g, '');
 
-    const example = `${data.list[0].example}`.replace(/\[|\]/g, '');
+  const example = `${data.list[0].example}`.replace(/\[|\]/g, '');
 
-    const upvotes = `${data.list[0].thumbs_up}`;
-    const downvotes = `${data.list[0].thumbs_down}`;
+  const upvotes = `${data.list[0].thumbs_up}`;
+  const downvotes = `${data.list[0].thumbs_down}`;
 
-    const response = stripIndents`**Definition for "${term}" ** (+${upvotes}/-${downvotes})
+  const response = stripIndents`**Definition for "${term}" ** (+${upvotes}/-${downvotes})
     ${definition}
     Example: ${example}`;
-    log.info(`[${PREFIX}] response: ${JSON.stringify(response, null, 2)}`);
-    return response;
+  log.info(`[${PREFIX}] response: ${JSON.stringify(response, null, 2)}`);
+  return response;
 }
