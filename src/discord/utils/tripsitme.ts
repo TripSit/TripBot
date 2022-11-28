37ad3ab412c8b6168797f9dafff74846ab864f76
spr1t3-0 <97351820+spr1t3-0@users.noreply.github.com>
2022-11-28 21:11:38 +0000

    minor fixes

--- src/discord/utils/tripsitme.ts
+++ src/discord/utils/tripsitme.ts
@@ -130,7 +130,7 @@ export async function needsHelpmode(
   // log.debug(`[${PREFIX}] targetHasRoleNeedshelp: ${targetHasRoleNeedshelp}`);
 
   // Save the user's roles to the DB
-  const targetRoleIds = target.roles.cache.map(role => role.id);
+  const targetRoleIds = target.roles.cache.map((role: { id: any; }) => role.id);
   // log.debug(`[${PREFIX}] targetRoleIds: ${targetRoleIds}`);
   await db<Users>('users')
     .insert({
@@ -143,13 +143,13 @@ export async function needsHelpmode(
   const myMember = await interaction.guild.members.fetch(interaction.client.user.id);
   const myRole = myMember.roles.highest;
   // Remove all roles, except team and vanity, from the target
-  target.roles.cache.forEach(role => {
+  target.roles.cache.forEach(async (role: { id: string; name: string | string[]; comparePositionTo: (arg0: any) => number; }) => {
     // log.debug(`[${PREFIX}] role: ${role.name} - ${role.id}`);
     if (!ignoredRoles.includes(role.id) && !role.name.includes('@everyone') && role.id !== roleNeedshelp.id) {
       if (role.comparePositionTo(myRole) < 0) {
         // log.debug(`[${PREFIX}] Removing role ${role.name} from ${target.displayName}`);
         try {
-          target.roles.remove(role);
+         await target.roles.remove(role);
         } catch (err) {
           // log.debug(`[${PREFIX}] There was an error removing the role ${role.name} from ${target.displayName}`);
         }
@@ -577,7 +577,7 @@ export async function tripsitmeResolve(
       if (roleNeedshelp.comparePositionTo(myRole) < 0) {
         try {
           // log.debug(`[${PREFIX}] Removing ${roleNeedshelp.name} from ${target.displayName}`);
-          target.roles.remove(roleNeedshelp);
+          await target.roles.remove(roleNeedshelp);
         } catch (err) {
           log.error(`[${PREFIX}] Error removing ${roleNeedshelp.name} from ${target.displayName}`);
           log.error(err);
@@ -600,7 +600,7 @@ export async function tripsitmeResolve(
           if (roleObj.comparePositionTo(myRole) < 0) {
             // log.debug(`[${PREFIX}] Adding role ${roleObj.name} to ${target.displayName}`);
             try {
-              target.roles.add(roleObj);
+             await target.roles.add(roleObj);
             } catch (err) {
               log.error(`[${PREFIX}] Error adding role ${roleObj.name} to ${target.displayName}`);
               log.error(err);
@@ -635,7 +635,7 @@ export async function tripsitmeResolve(
     After 7 days, or on request, it will be deleted to preserve your privacy =)`;
 
   try {
-    threadHelpUser.send(endHelpMessage);
+    await threadHelpUser.send(endHelpMessage);
   } catch (err) {
     log.error(`[${PREFIX}] Error sending end help message to ${threadHelpUser}`);
     log.error(err);
@@ -649,7 +649,7 @@ export async function tripsitmeResolve(
       > Thank you!
       ${env.EMOJI_INVISIBLE}
       `)
-    .then(async msg => {
+    .then(async (msg: { react: (arg0: string) => any; delete: () => void; }) => {
       message = msg;
       await msg.react('🙁');
       await msg.react('😕');
@@ -660,7 +660,7 @@ export async function tripsitmeResolve(
       // Setup the reaction collector
       const filter = (reaction:MessageReaction, user:User) => user.id === target.id;
       const collector = message.createReactionCollector({ filter, time: 1000 * 60 * 60 * 24 });
-      collector.on('collect', async reaction => {
+      collector.on('collect', async (reaction: { emoji: { name: any; }; }) => {
         threadHelpUser.send(stripIndents`
           ${env.EMOJI_INVISIBLE}
           > Thank you for your feedback, here's a cookie! 🍪
@@ -969,7 +969,7 @@ export async function tripsitmeButton(
   // but we'll change the output down below to make it clear this is a test.
   let targetIsTeamMember = false;
   if (!actorIsAdmin) {
-    target.roles.cache.forEach(async role => {
+    target.roles.cache.forEach(async (role: { id: string; }) => {
       if (teamRoles.includes(role.id)) {
         targetIsTeamMember = true;
       }
@@ -1091,7 +1091,7 @@ export async function tripsitmeButton(
 
   const filter = (i:ModalSubmitInteraction) => i.customId.startsWith('tripsitmeSubmit');
   interaction.awaitModalSubmit({ filter, time: 0 })
-    .then(async i => {
+    .then(async (i: { customId: string; fields: { getTextInputValue: (arg0: string) => any; }; }) => {
       if (i.customId.split('~')[1] !== interaction.id) return;
       const triage = i.fields.getTextInputValue('triageInput');
       const intro = i.fields.getTextInputValue('introInput');
