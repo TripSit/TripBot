4c6d14141fc5af781a8e119640416c9bf26edefb
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2022-12-05 08:03:12 -0600

    Remove role update logging cuz jfc

--- src/discord/events/roleUpdate.ts
+++ src/discord/events/roleUpdate.ts
@@ -1,13 +1,13 @@
-import {
-  TextChannel,
-} from 'discord.js';
-import {
-  AuditLogEvent,
-} from 'discord-api-types/v10';
+// import {
+//   TextChannel,
+// } from 'discord.js';
+// import {
+//   AuditLogEvent,
+// } from 'discord-api-types/v10';
 import {
   RoleUpdateEvent,
 } from '../@types/eventDef';
-import env from '../../global/utils/env.config';
+// import env from '../../global/utils/env.config';
 // import logger from '../../global/utils/logger';
 // import * as path from 'path';
 // const PREFIX = path.parse(__filename).name;
@@ -18,38 +18,40 @@ export default roleUpdate;
 
 export const roleUpdate: RoleUpdateEvent = {
   name: 'roleUpdate',
-  async execute(oldRole, newRole) {
+  async execute(/** oldRole, newRole* */) {
     // Only run on Tripsit, we don't want to snoop on other guilds ( ͡~ ͜ʖ ͡°)
-    if (newRole.guild.id !== env.DISCORD_GUILD_ID) {
-      return;
-    }
+    // if (newRole.guild.id !== env.DISCORD_GUILD_ID) {
+    //   return;
+    // }
 
-    const fetchedLogs = await newRole.guild.fetchAuditLogs({
-      limit: 1,
-      type: AuditLogEvent.RoleUpdate,
-    });
+    // This is super spammy, need to rework
 
-    // Since there's only 1 audit log entry in this collection, grab the first one
-    const auditLog = fetchedLogs.entries.first();
+    // const fetchedLogs = await newRole.guild.fetchAuditLogs({
+    //   limit: 1,
+    //   type: AuditLogEvent.RoleUpdate,
+    // });
 
-    const auditlog = client.channels.cache.get(env.CHANNEL_AUDITLOG) as TextChannel;
+    // // Since there's only 1 audit log entry in this collection, grab the first one
+    // const auditLog = fetchedLogs.entries.first();
 
-    // Perform a coherence check to make sure that there's *something*
-    if (!auditLog) {
-      await auditlog.send(`Role ${newRole.name} was updated, but no relevant audit logs were found.`);
-      return;
-    }
+    // const auditlog = client.channels.cache.get(env.CHANNEL_AUDITLOG) as TextChannel;
 
-    let response = '' as string;
-    const changes = auditLog.changes.map(change => `**[${change.key}]** '**${change.old}**' > '**${change.new}**'`);
-    if (auditLog.executor) {
-      response = `Role **${newRole.toString()}** was updated by ${auditLog.executor.tag}:`;
-      response += `\n${changes.join('\n')}`; // eslint-disable-line max-len
-    } else {
-      response = `Role ${newRole.toString()} was updated, but the audit log was inconclusive.`;
-      response += `\n${changes.join('\n')}`; // eslint-disable-line max-len
-    }
+    // // Perform a coherence check to make sure that there's *something*
+    // if (!auditLog) {
+    //   await auditlog.send(`Role ${newRole.name} was updated, but no relevant audit logs were found.`);
+    //   return;
+    // }
 
-    await auditlog.send(response);
+    // let response = '' as string;
+    // const changes = auditLog.changes.map(change => `**[${change.key}]** '**${change.old}**' > '**${change.new}**'`);
+    // if (auditLog.executor) {
+    //   response = `Role **${newRole.toString()}** was updated by ${auditLog.executor.tag}:`;
+    //   response += `\n${changes.join('\n')}`; // eslint-disable-line max-len
+    // } else {
+    //   response = `Role ${newRole.toString()} was updated, but the audit log was inconclusive.`;
+    //   response += `\n${changes.join('\n')}`; // eslint-disable-line max-len
+    // }
+
+    // await auditlog.send(response);
   },
 };
