fd2b39d3eff92fbc57633951c895818e0df15685
spr1t3-0 <ausfahrt@riseup.net>
2022-12-19 13:53:51 +0100

    add wikipedia command

---
+++ src/global/commands/g.wikipedia.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const F = f(__filename);
+
+export default wikipedia;
+
+/**
+ * Retrieve a definition from wikipedia
+ * @return {string}
+ */
+export async function wikipedia(query: string):Promise<string> {
+  try {
+    const response = await axios.get(
+      `https://en.wikipedia.org/api/rest_v1/page/summary/${query}`,
+    );
+    return response.data.extract;
+  } catch (e) {
+    log.error(F, `${(e as Error).message} query: ${query}`);
+    return `An error occured while trying to fetch the definition for ${query} from wikipedia.`;
+  }
+}
