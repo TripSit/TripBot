76abbdb0ccebaaccc46d24d63a5a91ee4ce65edb
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2022-12-24 10:23:41 -0600

    Move wiki test

--- jest/__tests__/__tests__todo__/wikipedia.test.ts
+++ jest/__tests__/__tests__todo__/wikipedia.test.ts
@@ -1,8 +1,8 @@
-import { dWikipedia } from '../../src/discord/commands/global/d.wikipedia';
-import { executeCommandAndSpyReply, embedContaining, getParsedCommand } from '../utils/testutils';
-import log from '../../src/global/utils/log'; // eslint-disable-line
 import { Colors } from 'discord.js';
 import { stripIndents } from 'common-tags';
+import { dWikipedia } from '../../../src/discord/commands/global/d.wikipedia';
+import { executeCommandAndSpyReply, embedContaining, getParsedCommand } from '../../utils/testutils';
+import log from '../../../src/global/utils/log'; // eslint-disable-line
 
 const slashCommand = dWikipedia;
 
@@ -29,14 +29,14 @@ describe(slashCommand.data.name, () => {
         iconURL: 'https://imgur.com/b923xK2.png',
         text: 'Dose responsibly!',
       },
-      title: 'Definition for T', 
-      description: stripIndents`T, or t, is the twentieth letter in the Latin alphabet, used in the modern English alphabet, the alphabets of other western European languages and others worldwide. Its name in English is tee, plural tees. It is derived from the Semitic Taw 𐤕 of the Phoenician and Paleo-Hebrew script via the Greek letter τ (tau). In English, it is most commonly used to represent the voiceless alveolar plosive, a sound it also denotes in the International Phonetic Alphabet. It is the most commonly used consonant and the second most commonly used letter in English-language texts.`
-    })) 
-    
+      title: 'Definition for T',
+      description: stripIndents`T, or t, is the twentieth letter in the Latin alphabet, used in the modern English alphabet, the alphabets of other western European languages and others worldwide. Its name in English is tee, plural tees. It is derived from the Semitic Taw 𐤕 of the Phoenician and Paleo-Hebrew script via the Greek letter τ (tau). In English, it is most commonly used to represent the voiceless alveolar plosive, a sound it also denotes in the International Phonetic Alphabet. It is the most commonly used consonant and the second most commonly used letter in English-language texts.`,
+    }));
+
     const spy2 = await executeCommandAndSpyReply(slashCommand, command2);
 
     expect(spy2).toHaveBeenCalledWith(embedContaining({
-      color: Colors.Purple, 
+      color: Colors.Purple,
       author: {
         iconURL: 'https://fossdroid.com/images/icons/me.tripsit.tripmobile.13.png',
         name: 'TripSit.Me',
@@ -46,12 +46,8 @@ describe(slashCommand.data.name, () => {
         iconURL: 'https://imgur.com/b923xK2.png',
         text: 'Dose responsibly!',
       },
-      title: 'Definition for thisissomenonsense', 
-      description: stripIndents`An error occured while trying to fetch the definition for thisissomenonsense from wikipedia.`
-    }))
-
+      title: 'Definition for thisissomenonsense',
+      description: stripIndents`An error occured while trying to fetch the definition for thisissomenonsense from wikipedia.`,
+    }));
   });
-
- 
-
 });
