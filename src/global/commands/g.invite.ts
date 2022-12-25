2c71e195b51d541dc9ffb35e812d55c7a108eb24
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2022-12-25 15:24:29 -0600

    Update dev invite

--- src/global/commands/g.invite.ts
+++ src/global/commands/g.invite.ts
@@ -9,10 +9,15 @@ export default invite;
  * @return {inviteInfo}
  */
 export async function invite():Promise<InviteInfo> {
-  const inviteInfo = {
-    bot: 'https://discord.com/api/oauth2/authorize?client_id=957780726806380545&permissions=18432&scope=bot%20applications.commands',
-    discord: 'https://discord.gg/TripSit',
-  };
+  const inviteInfo = process.env.NODE_ENV === 'production'
+    ? {
+      bot: 'https://discord.com/api/oauth2/authorize?client_id=957780726806380545&permissions=18432&scope=bot%20applications.commands',
+      discord: 'https://discord.gg/TripSit',
+    }
+    : {
+      bot: 'https://discord.com/api/oauth2/authorize?client_id=977945272359452713&permissions=18432&scope=bot%20applications.commands',
+      discord: 'https://discord.gg/cNDsrMSY',
+    };
   log.info(F, `response: ${JSON.stringify(inviteInfo, null, 2)}`);
   return inviteInfo;
 }
