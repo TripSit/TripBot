d4041f02f41ae1934bfd863cc33cfeaf885c15d8
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2023-01-11 18:44:45 -0600

    Balmer peak experimentation

--- src/global/utils/experience.ts
+++ src/global/utils/experience.ts
@@ -232,36 +232,36 @@ export async function experience(
               // if (totalData.level >= 60) {
               //   member.roles.remove(role);
               //   role = await guild.roles.fetch(env.ROLE_VIP_60) as Role;
-                // Beyond level 70 is not supported yet
-                // if (level >= 70) {
-                //   if (!member.roles.cache.has(role.id)) {
-                //     member.roles.remove(role);
-                //     channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
-                //   }
-                //   role = await guild.roles.fetch(env.ROLE_VIP_70) as Role;
-                //   if (level >= 80) {
-                //     if (!member.roles.cache.has(role.id)) {
-                //       member.roles.remove(role);
-                //       channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
-                //     }
-                //     role = await guild.roles.fetch(env.ROLE_VIP_80) as Role;
-                //     if (level >= 90) {
-                //       if (!member.roles.cache.has(role.id)) {
-                //         member.roles.remove(role);
-                //         channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
-                //       }
-                //       role = await guild.roles.fetch(env.ROLE_VIP_90) as Role;
-                //       if (level >= 100) {
-                //         if (!member.roles.cache.has(role.id)) {
-                //           member.roles.remove(role);
-                //           channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
-                //         }
-                //         role = await guild.roles.fetch(env.ROLE_VIP_100) as Role;
-                //       }
-                //     }
-                //   }
-                // }
-              }
+              // Beyond level 70 is not supported yet
+              // if (level >= 70) {
+              //   if (!member.roles.cache.has(role.id)) {
+              //     member.roles.remove(role);
+              //     channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
+              //   }
+              //   role = await guild.roles.fetch(env.ROLE_VIP_70) as Role;
+              //   if (level >= 80) {
+              //     if (!member.roles.cache.has(role.id)) {
+              //       member.roles.remove(role);
+              //       channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
+              //     }
+              //     role = await guild.roles.fetch(env.ROLE_VIP_80) as Role;
+              //     if (level >= 90) {
+              //       if (!member.roles.cache.has(role.id)) {
+              //         member.roles.remove(role);
+              //         channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
+              //       }
+              //       role = await guild.roles.fetch(env.ROLE_VIP_90) as Role;
+              //       if (level >= 100) {
+              //         if (!member.roles.cache.has(role.id)) {
+              //           member.roles.remove(role);
+              //           channelTripbotlogs.send(stripIndents`${actor.username} removed ${role.name}`);
+              //         }
+              //         role = await guild.roles.fetch(env.ROLE_VIP_100) as Role;
+              //       }
+              //     }
+              //   }
+              // }
+              // }
             }
           }
         }
