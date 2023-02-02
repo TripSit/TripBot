577c98e3952577edee6132550d340e8dcf4780a6
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2023-02-01 18:08:31 -0600

    Typo in rule 6b

--- src/discord/commands/global/d.setup.ts
+++ src/discord/commands/global/d.setup.ts
@@ -566,8 +566,8 @@ If you do not agree to this policy, do not use this site.
 
   await (interaction.channel as TextChannel).send({
     content: `
-0️⃣  Do not connect to TripSit or use our services if you are under eighteen.
-1️⃣  Do not use TripSit for any purpose or in any manner which could impair any other party's use or enjoyment of this site.
+1️⃣  Do not connect to TripSit or use our services if you are under eighteen.
+2️⃣  Do not use TripSit for any purpose or in any manner which could impair any other party's use or enjoyment of this site.
     a. Do not post anything considered offensive/upsetting to those in an altered mindset without a spoiler and content warning tag.
     b. Do not post images with the intent of causing disruption, including flashing imagery, spamming images, or multiple gifs.
     c. Do not post pornography (including softcore), gore, depictions of acts of violence, or other offensive content.
@@ -576,7 +576,7 @@ If you do not agree to this policy, do not use this site.
     f. Do not post content that victimizes, harasses, degrades, or intimidates an individual or group based on race, ethnicity, religion, sexual orientation, gender identification, drug of choice, level of addiction, mental health status, or other reasons.
     g. Do not post explicit content outside of the designated areas.
     h. Do not argue rules in public channels.
-2️⃣  Do not discuss, request, or post identifying information of websites, online vendors, or real-life people who sell or coordinate the purchase, distribution, or production of substances (legal, clearnet, or otherwise) or cryptocurrencies, i.e., no sourcing.
+3️⃣  Do not discuss, request, or post identifying information of websites, online vendors, or real-life people who sell or coordinate the purchase, distribution, or production of substances (legal, clearnet, or otherwise) or cryptocurrencies, i.e., no sourcing.
     a. Do not discuss the specifics or go in-depth into the mechanics of online vending.
     b. Do not show drug packaging to show how a vendor delivered something.
     `,
@@ -585,27 +585,27 @@ If you do not agree to this policy, do not use this site.
 
   await (interaction.channel as TextChannel).send({
     content: `
-3️⃣  Do not post any content that encourages, promotes, or signifies the intent to engage in unnecessarily harmful practices.
+4️⃣  Do not post any content that encourages, promotes, or signifies the intent to engage in unnecessarily harmful practices.
     a. Do not encourage substance use, and discourage drug dosages, drug combinations, or any drug experimentation which could be reliably considered unsafe.
     b. Do not post pictures or videos of drug consumption that glorify substance use, eg, “stash pics” or excessive dosages.
     c. Do not refuse to follow harm reduction standards, eg, continually engage in destructive behavior or refuse to call EMS when directed.
     d. Do not post suicide threats; our team is not qualified to manage this situation.
-4️⃣  Do not post intentionally libelous, defamatory, deceptive, fraudulent, tortious, or inaccurate content, i.e., misinformation.
-5️⃣  Do not post any content that is medical advice or imply that you can give medical advice.
+5️⃣  Do not post intentionally libelous, defamatory, deceptive, fraudulent, tortious, or inaccurate content, i.e., misinformation.
+6️⃣  Do not post any content that is medical advice or imply that you can give medical advice.
     a. Do not use “Dr” or “MD” in your nickname.`,
     flags: ['SuppressEmbeds'],
   });
 
   await (interaction.channel as TextChannel).send({
     content: `
-6️⃣  Do not post any content that violates another's privacy.
+7️⃣  Do not post any content that violates another's privacy.
     a. Do not post or collect personal information about channel members or doxing.
-    b. Do not post images of people expecting privacy or being unable to consent (children, intoxicated..
+    b. Do not post images of people expecting privacy or being unable to consent (children, intoxicated).
     c. Do not disseminate content originally posted in any team-only areas.
     d. Do not use network monitoring or discovery software to determine the site architecture or extract information about usage or users.
-7️⃣  Do not post content that bypasses moderation actions, i.e., ban evasion.
+8️⃣  Do not post content that bypasses moderation actions, i.e., ban evasion.
     a. Do not make multiple/new accounts, change your IP, or do anything else to get around the action.
-8️⃣  Do not post any content that links to other communication services, e.g., other discords, matrix servers, etc.`,
+9️⃣  Do not post any content that links to other communication services, e.g., other discords, matrix servers, etc.`,
     flags: ['SuppressEmbeds'],
   });
 }
