5a3684d2b0b78e0b0411a7b316e0a3407e0ea1ff
Hipperooni <hanzhanrob@gmail.com>
2023-02-10 12:38:26 +1100

    Fix status position

--- src/discord/commands/guild/d.profile.ts
+++ src/discord/commands/guild/d.profile.ts
@@ -249,7 +249,7 @@ export const dProfile: SlashCommand = {
     // Status Icon
     context.save();
     context.beginPath();
-    context.arc(109, 113, 21, 0, Math.PI * 2, true);
+    context.arc(110, 112, 21, 0, Math.PI * 2, true);
     context.closePath();
     context.fillStyle = cardDarkColor;
     context.fill();
@@ -282,7 +282,7 @@ export const dProfile: SlashCommand = {
 
     try {
       const StatusIcon = await Canvas.loadImage(StatusIconPath);
-      context.drawImage(StatusIcon, 88, 92);
+      context.drawImage(StatusIcon, 90, 92);
     } catch (err) {
       log.error(F, `Error loading status icon: ${err}`);
     }
@@ -426,7 +426,7 @@ export const dProfile: SlashCommand = {
     try {
       // log.debug(F, `LevelImagePath: ${LevelImagePath}`);
       const LevelImage = await Canvas.loadImage(LevelImagePath);
-      context.drawImage(LevelImage, 758, 57, 90, 90);
+      context.drawImage(LevelImage, 758, 57);
     } catch (err) {
       log.error(F, `Error loading star image: ${err}`);
     }
