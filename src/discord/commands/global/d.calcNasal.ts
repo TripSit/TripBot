4844b0b5459e5d3d2247c1d82b45afef0488eb37
spr1t3-0 <97351820+spr1t3-0@users.noreply.github.com>
2023-02-25 21:28:26 +0000

    fix

--- src/discord/commands/global/d.calcNasal.ts
+++ src/discord/commands/global/d.calcNasal.ts
@@ -6,11 +6,9 @@ import { SlashCommand } from '../../@types/commandDef';
 import { embedTemplate } from '../../utils/embedTemplate';
 import { calcSolvent, calcSubstance } from '../../../global/commands/g.calcNasal';
 
+export default dTemplate;
 
-export default dCalcNasal;
-
-export const dCalcNasal: SlashCommand = {
-
+export const dTemplate: SlashCommand = {
   data: new SlashCommandBuilder()
     .setName('calc_nasal') // there must be a better name for this
     .setDescription('Calculate how much of a substance or how much solvent you need to mix nose spray')
@@ -63,3 +61,4 @@ export const dCalcNasal: SlashCommand = {
     }
     return false;
   },
+};
