279bfcbde44929e5169938390d36c53e14b0b200
spr1t3-0 <97351820+spr1t3-0@users.noreply.github.com>
2023-02-26 09:25:19 +0000

    Fix UD Fixes #393

---
+++ src/discord/commands/global/d.urbanDefine.ts
@@ -0,0 +1,38 @@
+import {
+  SlashCommandBuilder,
+} from 'discord.js';
+import { stripIndents } from 'common-tags';
+import { SlashCommand } from '../../@types/commandDef';
+import { embedTemplate } from '../../utils/embedTemplate';
+import { urbandefine } from '../../../global/commands/g.urbandefine';
+import { startLog } from '../../utils/startLog';
+// import log from '../../../global/utils/log';
+const F = f(__filename);
+
+export default dUrbandefine;
+
+export const dUrbandefine: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName('urban_define')
+    .setDescription('Define a word on Urban Dictionary')
+    .addStringOption(option => option
+      .setName('define')
+      .setDescription('What do you want to define?')
+      .setRequired(true)),
+
+  async execute(interaction) {
+    startLog(F, interaction);
+    const term = interaction.options.getString('define');
+    if (!term) {
+      interaction.reply({ content: 'You must enter a search query.', ephemeral: true });
+      return false;
+    }
+
+    const result = await urbandefine(term);
+
+    const embed = embedTemplate()
+      .setDescription(stripIndents`${result}`);
+    interaction.reply({ embeds: [embed] });
+    return true;
+  },
+};
