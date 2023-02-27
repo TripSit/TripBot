08ef40ee9af5d992ed4e8fb9e17aef29482fbe5d
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2023-02-27 14:09:20 -0600

    Linting and typing

--- src/discord/commands/guild/d.trivia.ts
+++ src/discord/commands/guild/d.trivia.ts
@@ -3,50 +3,94 @@ import {
   SlashCommandBuilder,
   ButtonBuilder,
   ActionRowBuilder,
-  StringSelectMenuBuilder,
-  // ColorResolvable,
   MessageComponentInteraction,
-  time,
   ChatInputCommandInteraction,
   EmbedBuilder,
-  ModalBuilder,
-  TextInputBuilder,
-  ModalSubmitInteraction,
-  StringSelectMenuComponent,
-  // StringSelectMenuInteraction,
-  InteractionEditReplyOptions,
-  InteractionUpdateOptions,
-  SelectMenuComponentOptionData,
-  AttachmentBuilder,
   GuildMember,
-  TextChannel,
-  Message,
   ComponentType,
   ColorResolvable,
 } from 'discord.js';
 import {
-  APIEmbed,
-  APISelectMenuOption,
-  ButtonStyle, TextInputStyle,
+  ButtonStyle,
 } from 'discord-api-types/v10';
-import { stripIndents } from 'common-tags';
+import he from 'he';
 import { SlashCommand } from '../../@types/commandDef';
 import { embedTemplate } from '../../utils/embedTemplate';
 import { getPersonaInfo, setPersonaInfo } from '../../../global/commands/g.rpg';
 import { startLog } from '../../utils/startLog';
 import {
-  getUser, inventoryGet, inventorySet, personaSet,
+  getUser,
 } from '../../../global/utils/knex';
-import { Personas, RpgInventory } from '../../../global/@types/database';
-import { imageGet } from '../../utils/imageGet';
-const he = require('he')
+import { Personas } from '../../../global/@types/database';
+
 const Trivia = require('trivia-api');
+
 const trivia = new Trivia({ encoding: 'url3986' });
 const F = f(__filename);
 
-export async function getQuestions(amount: number, type: string) {
-  const { results } = await trivia({ amount, type });
-    return results;
+const optionDict = {
+  easy: {
+    name: 'Normal',
+    bonus: 1,
+  },
+  medium: {
+    name: 'Hard',
+    bonus: 1.5,
+  },
+  hard: {
+    name: 'Very Hard',
+    bonus: 2,
+  },
+};
+
+type TriviaQuestion = {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  all_answers: string[];
+};
+
+export async function getQuestions(
+  amount: number,
+  difficulty:string,
+):Promise<TriviaQuestion[]> {
+  log.debug(F, `Getting question with difficulty: ${difficulty}...`);
+
+  const { results } = await trivia.getQuestions({ amount, type: 'multiple', difficulty });
+
+  log.debug(F, `results: ${JSON.stringify(results, null, 2)}`);
+
+  return results.map((questionData:{
+    category: string;
+    type: string;
+    difficulty: string;
+    question: string;
+    correct_answer: string;
+    incorrect_answers: string[];
+  }) => {
+    const answers = [...questionData.incorrect_answers, questionData.correct_answer];
+    // Unescape HTML entities
+    const fixedQuestion = he.unescape(questionData.question);
+    // const fixedCorrectAnswer = he.unescape(question.correct_answer);
+    // const fixedIncorrectAnswers = he.unescape(...question.incorrect_answers);
+    const fixedAnswers = [...questionData.incorrect_answers, questionData.correct_answer];
+    // const fixedAnswers = he.unescape(answers)
+    log.debug(F, `Broken Question: ${questionData.question}, Fixed Question: ${fixedQuestion}`);
+    log.debug(F, `Broken Answer: ${answers}, Fixed Answer: ${fixedAnswers}`);
+    // Shuffle the answers (So the correct answer isn't always the last one)
+    fixedAnswers.sort(() => Math.random() - 0.5);
+
+    return {
+      category: questionData.category,
+      type: questionData.type,
+      difficulty: questionData.difficulty,
+      question: fixedQuestion,
+      correct_answer: questionData.correct_answer,
+      all_answers: fixedAnswers,
+    } as TriviaQuestion;
+  });
 }
 
 export const dTrivia: SlashCommand = {
@@ -71,23 +115,18 @@ export const dTrivia: SlashCommand = {
       )
       .setRequired(false)),
   async execute(interaction: ChatInputCommandInteraction) {
+    startLog(F, interaction);
 
-    const numberofQuestions = interaction.options.getString('amount') || '5';
-    const amountofQuestions = parseInt(numberofQuestions)
+    const amountOfQuestions = parseInt(interaction.options.getString('amount') || '5', 10);
     const chosenDifficulty = interaction.options.getString('difficulty') || 'easy';
-    const difficultyName = chosenDifficulty === 'easy' ? 'Normal' : chosenDifficulty === 'medium' ? 'Hard' : 'Very Hard';
-    const bonus = chosenDifficulty === 'easy' ? 1 : chosenDifficulty === 'medium' ? 1.5 : 2;
+    const difficultyName = optionDict[chosenDifficulty as keyof typeof optionDict].name;
+    const { bonus } = optionDict[chosenDifficulty as keyof typeof optionDict];
     let score = 0;
     let timedOut = false;
     let answerColor = Colors.Purple as ColorResolvable;
-    let embedStatus = `Starting trivia with ${numberofQuestions} questions!`;
+    let embedStatus = `Starting trivia with ${amountOfQuestions} questions!`;
     let questionAnswer = 'You have 30 seconds to answer each question.';
     const choices = ['A', 'B', 'C', 'D'];
-    const { results } = await trivia.getQuestions({
-      amount: amountofQuestions,
-      type: 'multiple', // Only multiple choice questions
-      difficulty: chosenDifficulty,
-    });
 
     // Get the user's persona data
     let [personaData] = await getPersonaInfo(interaction.user.id);
@@ -105,126 +144,114 @@ export const dTrivia: SlashCommand = {
 
       await setPersonaInfo(personaData);
     }
-  
-    for (let i = 0; (i < amountofQuestions); i++) {
+    const questionList = await getQuestions(amountOfQuestions, chosenDifficulty);
 
-      const question = results[0]; // Get the first question from the array
-      const answers = [...question.incorrect_answers, question.correct_answer]; // Combine the correct and incorrect answers
-      const fixedQuestion = he.unescape(question.question); // Unescape HTML entities
-      const fixedCorrectAnswer = he.unescape(question.correct_answer);
-      const fixedIncorrectAnswers = he.unescape(...question.incorrect_answers);
-      const fixedAnswers = [...question.incorrect_answers, question.correct_answer];
-      // const fixedAnswers = he.unescape(answers)
-      log.debug(F, `Broken Question: ${question.question}, Fixed Question: ${fixedQuestion}`)
-      log.debug(F, `Broken Answer: ${answers}, Fixed Answer: ${fixedAnswers}`)
-      answers.sort(() => Math.random() - 0.5); // Shuffle the answers (So the correct answer isn't always the last one)
-      const answerMap = new Map(fixedAnswers.map((answer, index) => [choices[index], `**${choices[index]}:** ${answer}`]));  // Map the answers to the choices (A, B, C, D)
+    for (let qNumber = 0; (qNumber < amountOfQuestions); qNumber += 1) {
+      // Get the first question from the array
+      const [questionData] = questionList;
+
+      const answerMap = new Map(questionData.all_answers.map((answer, index) => [choices[index], `**${choices[index]}:** ${answer}`])); // eslint-disable-line max-len
       const embed = new EmbedBuilder()
-        .setColor(answerColor as ColorResolvable)
+        .setColor(answerColor)
         .setTitle(`<:buttonTrivia:1079707985133191168> Trivia (${difficultyName})`)
-        .addFields({ name: `${embedStatus}`, value: `${questionAnswer}`})
-        .addFields({ name: `Question ${i + 1} of ${numberofQuestions}`, value: fixedQuestion })
+        .addFields({ name: `${embedStatus}`, value: `${questionAnswer}` })
+        .addFields({ name: `Question ${qNumber + 1} of ${amountOfQuestions}`, value: questionData.question })
         .addFields({ name: 'Choices', value: [...answerMap.values()].join('\n') })
-        .setFooter({ text: `${(interaction.member as GuildMember).displayName}'s TripSit RPG`, iconURL: env.TS_ICON_URL })
+        .setFooter({ text: `${(interaction.member as GuildMember).displayName}'s TripSit RPG`, iconURL: env.TS_ICON_URL }); // eslint-disable-line max-len
 
-      if (i === 0) {
-        await interaction.reply({ embeds: [embedTemplate().setTitle('Loading...')]})
-        await interaction.editReply({  // If it's the first question, send a new message
+      if (qNumber === 0) {
+        await interaction.reply({ embeds: [embedTemplate().setTitle('Loading...')] }); // eslint-disable-line no-await-in-loop, max-len
+        // If it's the first question, send a new message
+        await interaction.editReply({ // eslint-disable-line no-await-in-loop
           embeds: [embed],
           components: [
             new ActionRowBuilder<ButtonBuilder>().addComponents(
-              choices.map((choice) =>
-                new ButtonBuilder()
-                  .setCustomId(choice)
-                  .setLabel(choice)
-                  .setStyle(ButtonStyle.Success)
-              )
+              choices.map(choice => new ButtonBuilder()
+                .setCustomId(choice)
+                .setLabel(choice)
+                .setStyle(ButtonStyle.Success)),
             ),
           ],
         });
       } else {
-        await interaction.editReply({ // If not the first question, edit the previous message
+        // If not the first question, edit the previous message
+        await interaction.editReply({ // eslint-disable-line no-await-in-loop
           embeds: [embed],
         });
       }
 
-      const filter = (interaction: MessageComponentInteraction) => { // Filter for the buttons
-        return interaction.user.id === interaction.user.id && answerMap.has(interaction.customId);
-      };      
-
+      // Filter for the buttons
+      const filter = (i: MessageComponentInteraction) => i.user.id === interaction.user.id && answerMap.has(i.customId);
       try {
-        const collected = await interaction.channel!.awaitMessageComponent({
+        if (!interaction.channel) throw new Error('Channel not found');
+        const collected = await interaction.channel.awaitMessageComponent({ // eslint-disable-line no-await-in-loop
           filter,
           time: 30000,
           componentType: ComponentType.Button,
         });
 
         let answer = answerMap.get(collected.customId); // Get the answer from the map
-        answer = answer!.substring(7)
+        answer = answer?.substring(7);
         log.debug(F, `User chose: ${answer}`);
-        log.debug(F, `Correct answer was: ${question.correct_answer}`);
+        log.debug(F, `Correct answer was: ${questionData.correct_answer}`);
 
-        if (answer === question.correct_answer) {  // If the user answers correctly
+        if (answer === questionData.correct_answer) { // If the user answers correctly
           embedStatus = 'Correct!';
-          answerColor = Colors.Green as ColorResolvable,
-          questionAnswer = `The answer was **${question.correct_answer}**`;
-          score++;
-          await collected.update({
-          });
+          answerColor = Colors.Green as ColorResolvable;
+          questionAnswer = `The answer was **${questionData.correct_answer}**`;
+          score += 1;
+          await collected.update({}); // eslint-disable-line no-await-in-loop
         } else { // If the user answers incorrectly
           embedStatus = 'Incorrect!';
-          answerColor = Colors.Grey as ColorResolvable,
-          questionAnswer = `The correct answer was **${question.correct_answer}.**`;
-          await collected.update({
-          });
+          answerColor = Colors.Grey as ColorResolvable;
+          questionAnswer = `The correct answer was **${questionData.correct_answer}.**`;
+          await collected.update({}); // eslint-disable-line no-await-in-loop
         }
-
       } catch (error) { // If the user doesn't answer in time
-        embedStatus = `Time's up!`
-        answerColor = Colors.Red as ColorResolvable,
-        questionAnswer = `The correct answer was **${question.correct_answer}.**`
+        embedStatus = 'Time\'s up!';
+        answerColor = Colors.Red as ColorResolvable;
+        questionAnswer = `The correct answer was **${questionData.correct_answer}.**`;
         timedOut = true;
       }
 
-      results.splice(0, 1); // Remove the first question from the array
+      questionList.splice(0, 1); // Remove the first question from the array
       if (timedOut) break;
     }
 
-    let payout = 0
+    let payout = 0;
     if (score !== 0) { // The user won
-      payout = (score * bonus)
+      payout = (score * bonus);
       personaData.tokens += payout;
-      log.debug(F, `User scored: ${score}`)
-      log.debug(F, `User earned: ${payout} tokens`)
+      log.debug(F, `User scored: ${score}`);
+      log.debug(F, `User earned: ${payout} tokens`);
+      await setPersonaInfo(personaData);
     }
 
     if (!timedOut) {
       const embed = new EmbedBuilder()
         .setColor(Colors.Purple)
         .setTitle(`<:buttonTrivia:1079707985133191168> Trivia (${difficultyName})`)
-        .addFields({ name: `${embedStatus}`, value: `${questionAnswer}`})
-        .addFields({ name: `That's all the questions!`, value: ' '})
-        .addFields({ name: `You got ${score} out of ${numberofQuestions} questions correct, and earned ${payout} tokens!`, value: `You now have ${(personaData.tokens + payout)} tokens.`})
-        .setFooter({ text: `${(interaction.member as GuildMember).displayName}'s TripSit RPG`, iconURL: env.TS_ICON_URL })
+        .addFields({ name: `${embedStatus}`, value: `${questionAnswer}` })
+        .addFields({ name: 'That\'s all the questions!', value: ' ' })
+        .addFields({ name: `You got ${score} out of ${amountOfQuestions} questions correct, and earned ${payout} tokens!`, value: `You now have ${(personaData.tokens + payout)} tokens.` }) // eslint-disable-line max-len
+        .setFooter({ text: `${(interaction.member as GuildMember).displayName}'s TripSit RPG`, iconURL: env.TS_ICON_URL }); // eslint-disable-line max-len
       await interaction.editReply({
         embeds: [embed],
         components: [],
-      })
-      
+      });
     } else {
       const embed = new EmbedBuilder()
         .setColor(Colors.Purple)
         .setTitle(`<:buttonTrivia:1079707985133191168> Trivia (${difficultyName})`)
-        .addFields({ name: `${embedStatus}`, value: `${questionAnswer}`})
-        .addFields({ name: `Be faster next time!`, value: ' '})
-        .addFields({ name: `You got ${score} out of ${numberofQuestions} questions correct, and earned ${payout} tokens!`, value: `You now have ${(personaData.tokens + payout)} tokens.`})
-        .setFooter({ text: `${(interaction.member as GuildMember).displayName}'s TripSit RPG`, iconURL: env.TS_ICON_URL })
+        .addFields({ name: `${embedStatus}`, value: `${questionAnswer}` })
+        .addFields({ name: 'Be faster next time!', value: ' ' })
+        .addFields({ name: `You got ${score} out of ${amountOfQuestions} questions correct, and earned ${payout} tokens!`, value: `You now have ${(personaData.tokens + payout)} tokens.` }) // eslint-disable-line max-len
+        .setFooter({ text: `${(interaction.member as GuildMember).displayName}'s TripSit RPG`, iconURL: env.TS_ICON_URL }); // eslint-disable-line max-len
       await interaction.editReply({
         embeds: [embed],
         components: [],
-      })
+      });
     }
     return true;
-
   },
 };
