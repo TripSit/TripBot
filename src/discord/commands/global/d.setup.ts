f1dc11b32b9f16f81f4657f5f9ee244b708ce510
Hipperooni <hanzhanrob@gmail.com>
2023-02-28 00:29:15 +1100

    Update to rules formatting

--- src/discord/commands/global/d.setup.ts
+++ src/discord/commands/global/d.setup.ts
@@ -549,63 +549,74 @@ export async function rules(interaction:ChatInputCommandInteraction) {
   const rulesFileName = rulesPath.split('/').pop();
   // log.debug(F, `rulesFileName: ${rulesFileName}`);
 
-  const embed = embedTemplate()
-    .setAuthor(null)
-    .setFooter(null)
-    .setColor(Colors.Red)
-    .setImage(`attachment://${rulesFileName}`);
-  await (interaction.channel as TextChannel).send({ embeds: [embed], files: [rulesFile] });
-
   await (interaction.channel as TextChannel).send({
     content: `
 **By using and remaining connected to this discord you signify your agreement of TripSit's full terms and conditions: https://github.com/TripSit/rules/blob/main/termsofservice.md **
 If you do not agree to this policy, do not use this site.
+ \n 
+ `,
+    flags: ['SuppressEmbeds'],
+  });
+
+  await (interaction.channel as TextChannel).send({
+    content: `
+    \u200B
+    > 🔞 **1. Do not connect to TripSit or use our services if you are under eighteen.**
+
+    > 💞 **2. Do not use TripSit for any purpose or in any manner which could impair any other party's use or enjoyment of this site.**
+    > a. Do not post anything considered offensive/upsetting to those in an altered mindset without a spoiler and content warning tag.
+    > b. Do not post images with the intent of causing disruption, including flashing imagery, spamming images, or multiple gifs.
+    > c. Do not post pornography (including softcore), gore, depictions of acts of violence, or other offensive content.
+    > d. Do not display an offensive profile picture, including pornography of any kind.
+    > e. Do not use an offensive nickname or one that could cause anxiety in others, e.g., law enforcement or dictators.
+    > f. Do not post content that victimizes, harasses, degrades, or intimidates an individual or group based on race, ethnicity, religion, sexual orientation,  gender identification, drug of choice, level of addiction, mental health status, or other reasons.
+    
+    > 💊 **3. Do not discuss, request, or post identifying information of websites, online vendors, or real-life people who sell or coordinate the purchase, distribution, or production of substances (legal, clearnet, or otherwise) or cryptocurrencies, i.e., no sourcing.**
+    > a. Do not discuss the specifics or go in-depth into the mechanics of online vending.
+    > b. Do not show drug packaging to show how a vendor delivered something.
     `,
     flags: ['SuppressEmbeds'],
   });
 
   await (interaction.channel as TextChannel).send({
     content: `
-1️⃣  Do not connect to TripSit or use our services if you are under eighteen.
-2️⃣  Do not use TripSit for any purpose or in any manner which could impair any other party's use or enjoyment of this site.
-    a. Do not post anything considered offensive/upsetting to those in an altered mindset without a spoiler and content warning tag.
-    b. Do not post images with the intent of causing disruption, including flashing imagery, spamming images, or multiple gifs.
-    c. Do not post pornography (including softcore), gore, depictions of acts of violence, or other offensive content.
-    d. Do not display an offensive profile picture, including pornography of any kind.
-    e. Do not use an offensive nickname or one that could cause anxiety in others, e.g., law enforcement or dictators.
-    f. Do not post content that victimizes, harasses, degrades, or intimidates an individual or group based on race, ethnicity, religion, sexual orientation, gender identification, drug of choice, level of addiction, mental health status, or other reasons.
-    g. Do not post explicit content outside of the designated areas.
-    h. Do not argue rules in public channels.
-3️⃣  Do not discuss, request, or post identifying information of websites, online vendors, or real-life people who sell or coordinate the purchase, distribution, or production of substances (legal, clearnet, or otherwise) or cryptocurrencies, i.e., no sourcing.
-    a. Do not discuss the specifics or go in-depth into the mechanics of online vending.
-    b. Do not show drug packaging to show how a vendor delivered something.
+    \u200B
+    > 💀 **4. Do not post any content that encourages, promotes, or signifies the intent to engage in harmful practices.**
+    > a. Do not encourage substance use. Discourage drug dosages, drug combinations, or any drug experimentation which could be reliably considered unsafe.
+    > b. Do not post pictures or videos of drug consumption that glorify substance use, eg, “stash pics” or excessive dosages.
+    > c. Do not refuse to follow harm reduction standards, eg, continually engage in destructive behavior or refuse to call EMS when directed.
+    > d. Do not post suicide threats; our team is not qualified to manage this situation and will need to refer to Reddit's SuicideWatch community and suicide prevention hotlines by country: https://en.wikipedia.org/wiki/List_of_suicide_crisis_lines
+    
+    > ❌** 5. Do not post intentionally libellous, defamatory, deceptive, fraudulent, tortious, or inaccurate content, i.e., misinformation.**
+    
+    > 🥼 **6. Do not post any content that is medical advice or imply that you can give medical advice.**
+    > a. Do not use “Dr” or “MD” in your nickname.
+    
+    > 🔒 **7. Do not post any content that violates another's privacy.**
+    > a. Do not post or collect personal information about channel members or doxing.
+    > b. Do not post images of people expecting privacy or being unable to consent (children, intoxicated).
+    > c. Do not disseminate content originally posted in any team-only areas.
+    > d. Do not use network monitoring or discovery software to determine the site architecture or extract information about usage or users.
     `,
     flags: ['SuppressEmbeds'],
   });
 
   await (interaction.channel as TextChannel).send({
     content: `
-4️⃣  Do not post any content that encourages, promotes, or signifies the intent to engage in unnecessarily harmful practices.
-    a. Do not encourage substance use, and discourage drug dosages, drug combinations, or any drug experimentation which could be reliably considered unsafe.
-    b. Do not post pictures or videos of drug consumption that glorify substance use, eg, “stash pics” or excessive dosages.
-    c. Do not refuse to follow harm reduction standards, eg, continually engage in destructive behavior or refuse to call EMS when directed.
-    d. Do not post suicide threats; our team is not qualified to manage this situation.
-5️⃣  Do not post intentionally libelous, defamatory, deceptive, fraudulent, tortious, or inaccurate content, i.e., misinformation.
-6️⃣  Do not post any content that is medical advice or imply that you can give medical advice.
-    a. Do not use “Dr” or “MD” in your nickname.`,
-    flags: ['SuppressEmbeds'],
-  });
-
-  await (interaction.channel as TextChannel).send({
-    content: `
-7️⃣  Do not post any content that violates another's privacy.
-    a. Do not post or collect personal information about channel members or doxing.
-    b. Do not post images of people expecting privacy or being unable to consent (children, intoxicated).
-    c. Do not disseminate content originally posted in any team-only areas.
-    d. Do not use network monitoring or discovery software to determine the site architecture or extract information about usage or users.
-8️⃣  Do not post content that bypasses moderation actions, i.e., ban evasion.
-    a. Do not make multiple/new accounts, change your IP, or do anything else to get around the action.
-9️⃣  Do not post any content that links to other communication services, e.g., other discords, matrix servers, etc.`,
+     \u200B
+    > 🔨 **8. Do not post content that bypasses moderation actions, i.e., ban evasion.**
+    > a. Do not make multiple/new accounts, change your IP, or do anything else to get around the action.
+    
+    > 🔗 **9. Do not post any content that links to other communication services, e.g., other discords, matrix servers, etc.**
+    
+    > 🖼️ **10. Do not post any content that bypasses copyright laws.**
+    
+    > 💥 **11. Do not intentionally interfere with our services.**
+    
+    > 💰 **12. Do not use the content on this website for unapproved commercial exploitation.**
+    
+    > 📋 **13. Any unauthorized use of the Website or its Content is prohibited.**
+    `,
     flags: ['SuppressEmbeds'],
   });
 }
