beb3914c3aa1e8ffc6a437fabedd7f4a84629e9e
spr1t3-0 <97351820+spr1t3-0@users.noreply.github.com>
2023-03-05 17:59:40 +0000

    ToDo: meaningful commit message

--- src/matrix/commands/index.ts
+++ src/matrix/commands/index.ts
@@ -1,6 +1,10 @@
+/* eslint-disable import/no-cycle */
 /**
  * the exported name must always be lowercase
  */
 export * as drug from './m.drug';
 export * as combo from './m.combo';
 export * as breathe from './m.breathe';
+export * as calc_ketamine from './m.calc_ketamine';
+export * as grounding from './m.grounding';
+export * as combochart from './m.combochart';
