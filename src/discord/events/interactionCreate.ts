c0607023092dd086733ee2c82576f613ad9c2ac6
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2023-03-11 09:13:28 -0600

    Update interactionCreate with better logs

--- src/discord/events/interactionCreate.ts
+++ src/discord/events/interactionCreate.ts
@@ -12,7 +12,8 @@ import { commandRun } from '../utils/commandRun';
 import { buttonClick } from './buttonClick';
 import { selectMenu } from './selectMenu';
 import { autocomplete } from './autocomplete';
-import { getUser } from '../../global/utils/knex'; // eslint-disable-line
+import { db, getUser } from '../../global/utils/knex'; // eslint-disable-line
+import { Users } from '../../global/@types/database';
 
 const F = f(__filename);  // eslint-disable-line
 
@@ -21,34 +22,49 @@ export default interactionCreate;
 export const interactionCreate: InteractionCreateEvent = {
   name: 'interactionCreate',
   async execute(interaction) {
-    const userData = await getUser(interaction.user.id, null);
-    if (userData && userData.discord_bot_ban) {
+    const startTime = new Date().getTime();
+    log.debug(F, `${interaction.user.username} interactionCreate event started at ${startTime}`);
+    if (interaction.user.bot) return;
+    if (await db<Users>('users')
+      .select(db.ref('id').as('id'))
+      .where('discord_id', interaction.user.id)
+      .andWhere('discord_bot_ban', true)
+      .first()) {
       if (interaction.isRepliable()) {
         await interaction.reply({ content: '*beeps sadly*' });
       }
       return;
     }
+    log.debug(F, `Got user ban status in ${new Date().getTime() - startTime}ms`);
+
+    // const newStartTime = new Date().getTime();
+    // const userData = await getUser(interaction.user.id, null);
+    // if (userData && userData.discord_bot_ban) {
+    //   if (interaction.isRepliable()) {
+    //     await interaction.reply({ content: '*beeps sadly*' });
+    //   }
+    //   return;
+    // }
+    // log.debug(F, `(Legacy) Got user ban status in ${new Date().getTime() - newStartTime}ms`);
+
+    // Determine how long it took to get the banned status from the start time
     // log.debug(F, `interaction: ${JSON.stringify(interaction, null, 2)}`);
     // log.debug(F, `interaction: ${JSON.stringify(interaction)}`);
     // log.debug(F, `interaction: ${interaction}`);
     // log.debug(F, `typeof interaction: ${typeof interaction}`);
     // log.debug(F, `interaction.type: ${interaction.type}`);
 
-    if (interaction.user.bot) {
-      // log.debug(F, `Ignoring bot interaction`);
-      return;
-    }
-
     if (interaction.isChatInputCommand()) {
+      // Slash command
       // log.debug(F, `Interaction isChatInputCommand!`);
-
       commandRun(interaction, client);
       return;
     }
 
     if (interaction.type === InteractionType.ApplicationCommand
+      // Right click command
       && interaction.isContextMenuCommand()) {
-      // log.debug(F, `interaction.isContextMenuCommand(): ${interaction.isContextMenuCommand()}`);
+      log.debug(F, `interaction.isContextMenuCommand(): ${interaction.isContextMenuCommand()}`);
       commandRun(interaction, client);
       return;
     }
