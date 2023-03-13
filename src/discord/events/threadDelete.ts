84d731e827f73442f9c4bcd1965efc713dc47b4b
Moonbear <1836049+LunaUrsa@users.noreply.github.com>
2023-03-13 15:24:00 -0500

    Linting like a baus

--- src/discord/events/threadDelete.ts
+++ src/discord/events/threadDelete.ts
@@ -1,10 +1,4 @@
 import {
-  TextChannel,
-} from 'discord.js';
-import {
-  AuditLogEvent,
-} from 'discord-api-types/v10';
-import {
   ThreadDeleteEvent,
 } from '../@types/eventDef';
 import { getOpenTicket, ticketUpdate } from '../../global/utils/knex';
@@ -21,6 +15,20 @@ export default threadDelete;
 export const threadDelete: ThreadDeleteEvent = {
   name: 'threadDelete',
   async execute(thread) {
-    // Make this close tripsit threads
+    // Only run on Tripsit, we don't want to snoop on other guilds ( ͡~ ͜ʖ ͡°)
+    if (!thread.guild) return;
+    if (thread.guild.id !== env.DISCORD_GUILD_ID) return;
+    log.info(F, `Thread ${thread.name} was deleted.`);
+
+    // Find if the channel is used as a thread_id in any tickets
+    const ticketData = await getOpenTicket(null, thread.id);
+
+    if (ticketData) {
+      // log.debug(F, `closing ticket: ${JSON.stringify(ticketData, null, 2)}`);
+      // If it is, close the ticket
+
+      ticketData.status = 'DELETED' as TicketStatus;
+      await ticketUpdate(ticketData);
+    }
   },
 };
