04e788880783c1670e546ffac46d211b2c568441
Hipperooni <hanzhanrob@gmail.com>
2023-05-23 18:09:24 +1000

    Team Mindset Roles BETA
    It works, but is kinda slow sometimes, probably some inefficient looping

--- src/discord/events/roleAudit.ts
+++ src/discord/events/roleAudit.ts
@@ -24,6 +24,17 @@ const mindsetRoles = [
   env.ROLE_SOBER,
 ];
 
+const TTSmindsetRoles = [
+  env.ROLE_TTS_DRUNK,
+  env.ROLE_TTS_HIGH,
+  env.ROLE_TTS_ROLLING,
+  env.ROLE_TTS_TRIPPING,
+  env.ROLE_TTS_DISSOCIATING,
+  env.ROLE_TTS_STIMMING,
+  env.ROLE_TTS_SEDATED,
+  env.ROLE_TTS_SOBER,
+];
+
 const colorRoles = [
   env.ROLE_RED,
   env.ROLE_ORANGE,
@@ -92,13 +103,69 @@ export const guildMemberUpdate: GuildMemberUpdateEvent = {
               }
             }
           }
+          // Check if the id matches a mindsetRole
+          if (mindsetRoles.includes(roleId)) {
+            // log.debug(F, `mindset role added: ${roleId}`);
+            // If it does, check if the user also has team tripsit role
+            if (oldMember.roles.cache.has(env.ROLE_TEAMTRIPSIT))
+              // If so, replace the mindset role with the TTS equivalent
+              log.debug(F, `User added a mindset role while being a TTS!`);
+
+              // Find .env name of role using the mindsetRole table
+              // Iterate through the mindsetRoles array
+              for (const mindsetRole of mindsetRoles) {
+                // Check if the current mindsetRole matches the target ID
+                const userMindsetEnv = Object.keys(env).find((key) => env[key] === mindsetRole);
+                if (mindsetRole === roleId) {
+                  // The target ID matches the current mindsetRole object
+                  log.debug(F, `User Mindset role: ${userMindsetEnv}`)
+                  // Change "ROLE_" to "ROLE_TTS" in the .env name
+                  let ttsMindsetEnv = userMindsetEnv!.replace("ROLE_", "ROLE_TTS_");
+                  log.debug(F, `TTS mindset role: ${ttsMindsetEnv}`)
+                  // Find the role in the guild
+                  const ttsMindsetRole = oldMember.guild.roles.cache.get(env[ttsMindsetEnv]);
+                  log.debug(F, `TTS mindset role load: ${ttsMindsetRole}`)
+                  // If the role exists, add it to the user
+                  if (ttsMindsetRole) {
+                    await newMember.roles.add(ttsMindsetRole);
+                  break; // Exit the loop since we found the desired object
+                }
+              }
+            }
+          }
         });
       }
       if (rolesRemoved.length > 0) {
         if (newMember.guild.id !== env.DISCORD_GUILD_ID) return;
         // log.debug(F, `roles removed: ${rolesRemoved}`);
-        // Go through each role removed, and check if the id matches a donorRole
+        // Go through each role removed
         rolesRemoved.forEach(async roleId => {
+          // Check if it's a mindsetRole
+          if (mindsetRoles.includes(roleId) && newMember.roles.cache.has(env.ROLE_TEAMTRIPSIT)) {
+            // Remove all TTS mindsets
+            // Check if the member has any of the TTSmindsetRoles
+            for (const mindsetRole of mindsetRoles) {
+              // Check if the current mindsetRole matches the target ID
+              const userMindsetEnv = Object.keys(env).find((key) => env[key] === mindsetRole);
+              if (mindsetRole === roleId) {
+                // The target ID matches the current mindsetRole object
+                log.debug(F, `User Mindset role: ${userMindsetEnv}`)
+                // Change "ROLE_" to "ROLE_TTS" in the .env name
+                let ttsMindsetEnv = userMindsetEnv!.replace("ROLE_", "ROLE_TTS_");
+                log.debug(F, `TTS mindset role: ${ttsMindsetEnv}`)
+                // Find the role in the guild
+                const ttsMindsetRole = oldMember.guild.roles.cache.get(env[ttsMindsetEnv]);
+                log.debug(F, `TTS mindset role load: ${ttsMindsetRole}`)
+                // If the role exists, add it to the user
+                if (ttsMindsetRole) {
+                  await newMember.roles.remove(ttsMindsetRole);
+                break; // Exit the loop since we found the desired object
+              }
+            }
+          }
+          }
+
+          // Check if it's a donor role
           if (donorRoles.includes(roleId)) {
             // log.debug(F, `donor role removed: ${roleId}`);
             // If it does, check if the user also has a role id matching a donorColorRole and if so, remove it
