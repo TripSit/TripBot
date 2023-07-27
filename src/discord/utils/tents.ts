dae1c60c35e60f56c5c0f3e66ca6f949aaf76b62
LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>
2023-07-27 15:11:28 -0500

    Minor update to tent intro

--- src/discord/utils/tents.ts
+++ src/discord/utils/tents.ts
@@ -38,6 +38,8 @@ export async function pitchTent(
 - **Looking for others to join?**
  - Pick up the 'Voice Chatty' role in <id:customize>
  - This icon indicates you're looking for joiners in chat
+ - You can (infrequently) mention the \`@Join VC\` role to see if anyone wants to join!
+ - You can pick up this role in <id:customize>
 
 - **Moderate your tent with commands**
  - \`/voice lock\`- Locks your tent so no one else can join it
