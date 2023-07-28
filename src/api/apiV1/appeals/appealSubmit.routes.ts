20522141600eccce8e4fec61252789ad4b0e7350
LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>
2023-07-28 08:37:43 -0500

    Fix rate limiter

--- src/api/apiV1/appeals/appealSubmit.routes.ts
+++ src/api/apiV1/appeals/appealSubmit.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import RateLimit from 'express-rate-limit';
 import checkAuth from '../../utils/checkAuth';
 
 import appeals from './appealSubmit.queries';
@@ -6,6 +7,15 @@ import appeals from './appealSubmit.queries';
 const F = f(__filename); // eslint-disable-line
 
 const router = express.Router();
+
+// set up rate limiter: maximum of five requests per minute
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
+// apply rate limiter to all requests
+router.use(limiter);
 
 router.post('/', async (req, res, next) => {
   if (await checkAuth(req, res)) {
