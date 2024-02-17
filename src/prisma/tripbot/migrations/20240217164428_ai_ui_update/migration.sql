8e3e151c8274adfe1ab4e28edb077249fce2738d
LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>
2024-02-17 10:45:02 -0600

    Redo entire migration file ig

--- src/prisma/tripbot/migrations/20240217164428_ai_ui_update/migration.sql
+++ src/prisma/tripbot/migrations/20240217164428_ai_ui_update/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - The values [GPT-4,DAVINCI,CURIE,BABBAGE,ADA,GPT-3.5-TURBO-1106,GPT-4-1106-PREVIEW,GPT-4-1106-VISION-PREVIEW,DALL-E-2] on the enum `ai_model` will be removed. If these variants are still used in the database, this will fail.
-  - A unique constraint covering the columns `[channel_id]` on the table `ai_channels` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `guild_id` to the `ai_channels` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -15,12 +14,6 @@ ALTER TYPE "ai_model" RENAME TO "ai_model_old";
 ALTER TYPE "ai_model_new" RENAME TO "ai_model";
 DROP TYPE "ai_model_old";
 COMMIT;
-
--- Drop the unique constraint
-ALTER TABLE ai_channels DROP CONSTRAINT aichannels_channelid_personaid_unique;
-
--- DropIndex
-DROP INDEX "aichannels_channelid_personaid_unique";
 
 -- AlterTable
 ALTER TABLE "ai_channels" ADD COLUMN     "guild_id" TEXT NOT NULL;
@@ -35,9 +28,3 @@ ADD COLUMN     "upvotes" INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE "users" ADD COLUMN     "ai_history_google" TEXT,
 ADD COLUMN     "ai_history_openai" TEXT,
 ADD COLUMN     "ai_terms_agree" BOOLEAN NOT NULL DEFAULT false;
-
--- CreateIndex
-CREATE UNIQUE INDEX "aichannels_channelid_unique" ON "ai_channels"("channel_id");
-
--- AddForeignKey
-ALTER TABLE "ai_channels" ADD CONSTRAINT "aichannels_guildid_foreign" FOREIGN KEY ("guild_id") REFERENCES "discord_guilds"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
