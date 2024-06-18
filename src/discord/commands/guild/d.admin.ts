1e4df9c789af1a13d6007c8f9bdce652a03ee0c5
Hipperooni <60094069+Hipperooni@users.noreply.github.com>
2024-06-18 23:08:34 +1000

    Add ability to change bot's banner, avatar and status (including gifs) (#790)
    Co-authored-by: LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>

--- src/discord/commands/guild/d.admin.ts
+++ src/discord/commands/guild/d.admin.ts
@@ -2,6 +2,7 @@ import {
   ChatInputCommandInteraction,
   SlashCommandBuilder,
   TextChannel,
+  ActivityType,
 } from 'discord.js';
 import axios from 'axios';
 import { stripIndents } from 'common-tags';
@@ -65,6 +66,84 @@ async function deploy(
     ${commandData.globalCommands.length} global commands and ${commandData.guildCommands.length} guild commands`);
 }
 
+async function setAvatar(interaction: ChatInputCommandInteraction, avatarUrl: string): Promise<boolean> {
+  const response = await axios.get(avatarUrl, { responseType: 'arraybuffer' });
+  const base64 = Buffer.from(response.data, 'binary').toString('base64');
+  const data = `data:image/jpeg;base64,${base64}`;
+
+  await axios.patch('https://discord.com/api/v10/users/@me', {
+    avatar: data,
+  }, {
+    headers: {
+      Authorization: `Bot ${interaction.client.token}`,
+      'Content-Type': 'application/json',
+    },
+  }).then(() => {
+    console.log('Avatar set successfully');
+    interaction.editReply('Avatar set successfully');
+    return true;
+  }).catch((error: Error) => {
+    console.error(`Error setting avatar: ${error.message}`);
+    interaction.editReply('Error setting avatar');
+    return false;
+  });
+  return false;
+}
+
+async function setBanner(interaction: ChatInputCommandInteraction, bannerUrl: string): Promise<boolean> {
+  const response = await axios.get(bannerUrl, { responseType: 'arraybuffer' });
+  const base64 = Buffer.from(response.data, 'binary').toString('base64');
+  const data = `data:image/jpeg;base64,${base64}`;
+
+  await axios.patch('https://discord.com/api/v10/users/@me', {
+    banner: data,
+  }, {
+    headers: {
+      Authorization: `Bot ${interaction.client.token}`,
+      'Content-Type': 'application/json',
+    },
+  }).then(() => {
+    console.log('Banner set successfully');
+    interaction.editReply('Banner set successfully');
+    return true;
+  }).catch((error: Error) => {
+    console.error(`Error setting banner: ${error.message}`);
+    interaction.editReply('Error setting banner');
+    return false;
+  });
+  return false;
+}
+
+async function setStatus(
+  interaction: ChatInputCommandInteraction,
+  prefix: string,
+  status: string,
+): Promise<void> {
+  let statusType: ActivityType;
+  switch (prefix) {
+    case 'none':
+    case 'playing':
+      statusType = ActivityType.Playing;
+      break;
+    case 'streaming':
+      statusType = ActivityType.Streaming;
+      break;
+    case 'listening':
+      statusType = ActivityType.Listening;
+      break;
+    case 'watching':
+      statusType = ActivityType.Watching;
+      break;
+    case 'competing':
+      statusType = ActivityType.Competing;
+      break;
+    default:
+      statusType = ActivityType.Playing;
+  }
+  await interaction.client.user?.setActivity(status, { type: statusType });
+  await interaction.editReply(`Status set to ${statusType} ${status}`);
+}
+
 export const dAdmin: SlashCommand = {
   data: new SlashCommandBuilder()
     .setName('admin')
@@ -77,12 +156,43 @@ export const dAdmin: SlashCommand = {
       .setDescription('Rebuild the bot'))
     .addSubcommand(subcommand => subcommand
       .setName('deploy')
-      .setDescription('Deploy commands')),
+      .setDescription('Deploy commands'))
+    .addSubcommand(subcommand => subcommand
+      .setName('setstatus')
+      .setDescription('Set the bot\'s status')
+      .addStringOption(option => option
+        .setName('prefix')
+        .setDescription('The prefix')
+        .addChoices(
+          { name: 'Playing', value: 'playing' },
+          { name: 'Listening', value: 'listening' },
+          { name: 'Watching', value: 'watching' },
+          { name: 'Competing', value: 'competing' },
+        )
+        .setRequired(true))
+      .addStringOption(option => option
+        .setName('status')
+        .setDescription('The status text')
+        .setRequired(true)))
+    .addSubcommand(subcommand => subcommand
+      .setName('setavatar')
+      .setDescription('Set the bot\'s avatar')
+      .addStringOption(option => option
+        .setName('url')
+        .setDescription('The URL of the avatar')
+        .setRequired(true)))
+    .addSubcommand(subcommand => subcommand
+      .setName('setbanner')
+      .setDescription('Set the bot\'s banner')
+      .addStringOption(option => option
+        .setName('url')
+        .setDescription('The URL of the banner')
+        .setRequired(true))),
   async execute(interaction) {
     if (!interaction.channel) return false;
     if (!interaction.guild) return false;
     log.info(F, await commandContext(interaction));
-    const command = interaction.options.getSubcommand() as 'restart' | 'rebuild' | 'deploy';
+    const command = interaction.options.getSubcommand() as 'restart' | 'rebuild' | 'deploy' | 'setstatus' | 'setavatar' | 'setbanner';
     // By default we want to make the reply private
     await interaction.deferReply({ ephemeral: true });
     // eslint-disable-next-line sonarjs/no-small-switch
@@ -97,6 +207,18 @@ export const dAdmin: SlashCommand = {
       }
       case 'deploy': {
         await deploy(interaction);
+        break;
+      }
+      case 'setavatar': {
+        await setAvatar(interaction, interaction.options.getString('url') as string);
+        break;
+      }
+      case 'setbanner': {
+        await setBanner(interaction, interaction.options.getString('url') as string);
+        break;
+      }
+      case 'setstatus': {
+        await setStatus(interaction, interaction.options.getString('prefix') as string, interaction.options.getString('status') as string);
         break;
       }
       default: {
