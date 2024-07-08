a39998372d8520defdf99204c0bef05ceb027ee7
Shadow <blazeis292@gmail.com>
2024-07-08 07:32:46 -0400

    feat(translate): Adds translate feature

---
+++ src/discord/commands/global/m.translate.ts
@@ -0,0 +1,58 @@
+import {
+  ContextMenuCommandBuilder,
+  Colors,
+} from 'discord.js';
+import {
+  ApplicationCommandType,
+} from 'discord-api-types/v10';
+import OpenAI from 'openai';
+import { MessageCommand } from '../../@types/commandDef';
+import { embedTemplate } from '../../utils/embedTemplate';
+import { aiTranslate } from '../../../global/commands/g.ai';
+
+const F = f(__filename);
+
+export const mTranslate: MessageCommand = {
+  data: new ContextMenuCommandBuilder()
+    .setName('Translate')
+    .setType(ApplicationCommandType.Message),
+  async execute(interaction) {
+    if (!interaction.guild) return false;
+    log.info(F, await commandContext(interaction));
+    await interaction.deferReply({ ephemeral: true });
+
+    // const target_language = interaction.options.getString('target_language');
+
+    // if (!target_language) {
+    //   await interaction.editReply({
+    //     embeds: [embedTemplate()
+    //       .setTitle('Error!')
+    //       .setDescription('You must include target_language!')
+    //       .setColor(Colors.Red)
+    //       .setFooter(null)],
+    //   });
+    //   return false;
+    // }
+
+    const targetMessage = interaction.targetMessage.content;
+
+    const messageList = [{
+      role: 'user',
+      content: targetMessage,
+    }] as OpenAI.Chat.ChatCompletionMessageParam[];
+    log.info(F, targetMessage);
+    // There may be the potential for the user to use this to make TripBot say bad things. 
+    // Do we need some kind of filter?
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { response, promptTokens, completionTokens } = await aiTranslate('English', messageList);
+    await interaction.editReply({
+      embeds: [embedTemplate()
+        .setTitle('Here\'s your translation!')
+        .setDescription(response)
+        .setColor(Colors.Blurple)],
+    });
+    return true;
+  },
+};
+
+export default mTranslate;
