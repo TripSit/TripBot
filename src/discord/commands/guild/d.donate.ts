f96e93d6ed85887295363a15b6898e183cae55f5
Shadow <blazeis292@gmail.com>
2024-07-30 13:15:59 -0400

    donate opens help, exported help pages

--- src/discord/commands/guild/d.donate.ts
+++ src/discord/commands/guild/d.donate.ts
@@ -4,6 +4,7 @@ import {
 import { SlashCommand } from '../../@types/commandDef';
 import commandContext from '../../utils/context';
 import { embedTemplate } from '../../utils/embedTemplate';
+import { donatePage } from '../global/d.help';
 
 const F = f(__filename);
 
@@ -17,10 +18,12 @@ export const dDonate: SlashCommand = {
     log.info(F, await commandContext(interaction));
     await interaction.deferReply({ ephemeral: (interaction.options.getBoolean('ephemeral') === true) });
 
-    const embed = embedTemplate()
-      .setTitle('Learn more about supporting TripSit')
-      .setFooter({ text: 'This command is deprecated. Please use /help and scroll down to the Support TripSit page. Thank you! <3 ' }); // eslint-disable-line max-len
-    await interaction.editReply({ embeds: [embed] });
+    donatePage();
+    // const embed = embedTemplate()
+    //   .setTitle('Learn more about supporting TripSit')
+    //   .setFooter({ text: 'This command is deprecated. Please use /help and scroll down to the Support TripSit page. Thank you! <3 ' }); // eslint-disable-line max-len
+    // await interaction.editReply({ embeds: [embed] });
+    await interaction.editReply(await donatePage());
     return true;
   },
 };
