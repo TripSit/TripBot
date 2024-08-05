38c3942e3e549a58c3bb1a151177610254c45f22
Shadow <blazeis292@gmail.com>
2024-08-05 14:59:29 +0000

    Update to new reagents image and start using repo for image hosting (#826)
    * chore(reagents): switch to repo images n update reagents
    
    * testing
    
    * final version
    
    ---------
    
    Co-authored-by: LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>

--- src/global/commands/g.reagents.ts
+++ src/global/commands/g.reagents.ts
@@ -9,7 +9,7 @@ export default reagents;
 export async function reagents():Promise<string> {
   // const response = 'https://i.imgur.com/wETJsZr.png';
   // eslint-disable-next-line max-len
-  const response = 'https://user-images.githubusercontent.com/1836049/222908130-df3a881b-3ced-462f-a0db-6c2a34cd62ec.png';
+  const response = 'https://raw.githubusercontent.com/TripSit/TripBot/main/.github/images/reagents.png';
   log.info(F, `response: ${JSON.stringify(response, null, 2)}`);
   return response;
 }
