df3636ce04a8a3855faa283c33634af5e56683c9
Shadow <blazeis292@gmail.com>
2024-08-05 09:58:34 -0400

    testing

--- src/global/commands/g.reagents.ts
+++ src/global/commands/g.reagents.ts
@@ -9,7 +9,8 @@ export default reagents;
 export async function reagents():Promise<string> {
   // const response = 'https://i.imgur.com/wETJsZr.png';
   // eslint-disable-next-line max-len
-  const response = 'https://raw.githubusercontent.com/TripSit/TripBot/reagents-update/.github/images/reagents.png';
-  log.info(F, `response: ${JSON.stringify(response, null, 2)}`);
-  return response;
+  const response1 = 'https://raw.githubusercontent.com/theimperious1/TripBot/reagents-update/.github/images/reagents.png';
+  // const response = 'https://raw.githubusercontent.com/TripSit/TripBot/reagents-update/.github/images/reagents.png';
+  log.info(F, `response: ${JSON.stringify(response1, null, 2)}`);
+  return response1;
 }
