6ee9764d6f81752f53fd48da4f02729e2fcdf8fd
theimperious1 <blazeis292@gmail.com>
2024-10-03 13:46:38 -0400

    Add default value for new column last_helper_activity

--- src/prisma/tripbot/migrations/20240930194441_auto_prune_helper/migration.sql
+++ src/prisma/tripbot/migrations/20240930194441_auto_prune_helper/migration.sql
@@ -1,2 +1,3 @@
 -- AlterTable
-ALTER TABLE "users" ADD COLUMN     "last_helper_activity" TIMESTAMP(3);
+ALTER TABLE "users" 
+ADD COLUMN "last_helper_activity" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP;
