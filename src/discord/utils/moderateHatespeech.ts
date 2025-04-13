1a44b67431a8aa8adead5df73737634b3a8cf3aa
theimperious1 <shadow@tripsit.me>
2025-04-13 12:51:14 -0400

    Better ai mod log. Clean up messageCreate. Fix tokens to work on prod and dev ideally by modifying env

--- src/discord/utils/moderateHatespeech.ts
+++ src/discord/utils/moderateHatespeech.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Colors, Message, TextChannel } from 'discord.js';
 
 const F = f(__filename);
 const MODERATION_API_URL = 'https://api.moderatehatespeech.com/api/v1/moderate/';
@@ -12,10 +13,10 @@ const MODERATION_API_URL = 'https://api.moderatehatespeech.com/api/v1/moderate/'
  * @returns boolean - True if the text is considered toxic or uncertain; false otherwise.
  */
 export async function isToxic(text: string): Promise<boolean> {
-  const apiKey = env.MODERATE_HATESPEECH_API_KEY;
-  const minConfidence = env.MODERATE_HATESPEECH_NORMAL_CONFIDENCE;
+  const apiKey = process.env.MODERATE_HATESPEECH_TOKEN;
+  const minNormalConfidence = env.MODERATE_HATESPEECH_NORMAL_CONFIDENCE;
 
-  if (!apiKey || !minConfidence) {
+  if (!apiKey || !minNormalConfidence) {
     log.error(F, 'ModerationHatespeech API key or confidence level not set');
     return false;
   }
@@ -29,13 +30,28 @@ export async function isToxic(text: string): Promise<boolean> {
 
     log.info(F, `ModerateHatespeech API response: ${JSON.stringify(data)}`);
 
-    const isNormalWithLowConfidence = data.class === 'normal' && data.confidence < minConfidence;
+    const isNormalWithLowConfidence = data.class === 'normal' && data.confidence < minNormalConfidence;
     const isFlagged = data.class === 'flag';
 
     return isNormalWithLowConfidence || isFlagged;
   } catch (error) {
     log.error(F, `Moderation API error: ${error}`);
     return false;
+  }
+}
+
+export async function monitorToxicity(message: Message): Promise<void> {
+  if (await isToxic(message.content)) {
+    log.info(F, `Message flagged by ModerateHatespeech: "${message.content}"`);
+    const logChannelId = env.NODE_ENV === 'production' ? env.CHANNEL_AIMOD_LOG : env.CHANNEL_BOTSPAM;
+    const aiModLogChannel = await discordClient.channels.fetch(logChannelId) as TextChannel;
+    await aiModLogChannel.send({
+      embeds: [{
+        title: 'Flagged by ModerateHatespeech',
+        description: `**Author:** ${message.author.displayName}\n**Message:** ${message.content}\n**Link:** ${message.url}`,
+        color: Colors.Yellow,
+      }],
+    });
   }
 }
 
