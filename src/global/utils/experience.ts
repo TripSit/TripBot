8256291649355d48d2f69e74127bd3103afec3c9
theimperious1 <shadow@tripsit.me>
2025-05-28 15:53:35 -0400

    Add more debug logs and try cross-references to BountyAwards.yml

--- src/global/utils/experience.ts
+++ src/global/utils/experience.ts
@@ -320,6 +320,7 @@ export async function experience(
       } else if (category === 'IGNORED' as experience_category) {
         id = env.CHANNEL_BOTSPAM;
       }
+      // log.debug(F, `id: ${id}`);
       sendMilestoneAnnouncement(member, id, experienceData.level, categoryName, typeName);
     }
   }
