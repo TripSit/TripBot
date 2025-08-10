46c640f317c022bea86bcfa3d7649e8db014d3f0
theimperious1 <shadow@tripsit.me>
2025-08-10 17:00:03 -0400

    disable basic auth for now. need to use keycloak later somehow or do something secure.

--- src/api/v2/appeals/appeals.routes.ts
+++ src/api/v2/appeals/appeals.routes.ts
@@ -20,102 +20,88 @@ const limiter = RateLimit({
 router.use(limiter);
 
 router.get('/:userId/latest', async (req, res, next) => {
-  if (await checkAuth(req, res)) {
-    const { userId } = req.params;
-    log.debug(F, `userId: ${userId}`);
-    try {
-      if (userId === 'error') throw new Error('error');
-      const result = await appeals.getLatestAppeal(userId);
-      if (result) {
-        return res.json(result);
-      }
-      return next();
-    } catch (error) {
-      return next(error);
+  const { userId } = req.params;
+  log.debug(F, `userId: ${userId}`);
+  try {
+    if (userId === 'error') throw new Error('error');
+    const result = await appeals.getLatestAppeal(userId);
+    if (result) {
+      return res.json(result);
     }
-  } else {
     return next();
+  } catch (error) {
+    return next(error);
   }
 });
 
 router.post('/:userId/create', async (req, res, next) => {
-  if (await checkAuth(req, res)) {
-    try {
-      if (req.params.userId === 'error') throw new Error('error');
-      const appealData = req.body.newAppealData as AppealData;
-      const result = await appeals.createAppeal({
-        guild_id: appealData.guild,
-        user_id: req.params.userId,
-        reason: appealData.reason,
-        solution: appealData.solution,
-        future: appealData.future,
-        extra: appealData.extra,
-        appeal_message_id: '54321',
-      });
+  try {
+    if (req.params.userId === 'error') throw new Error('error');
+    const appealData = req.body.newAppealData as AppealData;
+    const result = await appeals.createAppeal({
+      guild_id: appealData.guild,
+      user_id: req.params.userId,
+      reason: appealData.reason,
+      solution: appealData.solution,
+      future: appealData.future,
+      extra: appealData.extra,
+      appeal_message_id: '54321',
+    });
 
-      if (result) {
-        const botMember = await discordClient.guilds.cache.first()?.members.fetch(discordClient.user!.id);
-        if (!botMember) throw new Error("Failed to fetch bot user.");
-    
-        // Fetch the target user
-        const targetUser = await discordClient.users.fetch(appealData.userId);
-        if (!targetUser) throw new Error("Failed to fetch target user.");
+    if (result) {
+      const botMember = await discordClient.guilds.cache.first()?.members.fetch(discordClient.user!.id);
+      if (!botMember) throw new Error('Failed to fetch bot user.');
 
-        const description = `
-        **Do you know why you were banned?**: ${appealData.reason}
+      // Fetch the target user
+      const targetUser = await discordClient.users.fetch(appealData.userId);
+      if (!targetUser) throw new Error('Failed to fetch target user.');
 
-          **Have you taken any steps to rectify the situation or make amends for the behavior that lead to the ban?**: ${appealData.solution}
+      const description = `
+      **Do you know why you were banned?**: ${appealData.reason}
 
-          **What steps will you take to ensure that you do not repeat the behavior that lead to the ban?**: ${appealData.future}
+        **Have you taken any steps to rectify the situation or make amends for the behavior that lead to the ban?**: ${appealData.solution}
 
-          **Is there anything else you would like to add?**: ${appealData.extra}
-          `;
-        await messageModThread(
-          null,
-          botMember,
-          targetUser,
-          'BAN_APPEAL',
-          '',
-          description,
-          '',
-          '',
-          appealData,
-        );
-        return res.json(result);
-      }
-      return next();
-    } catch (error) {
-      return next(error);
+        **What steps will you take to ensure that you do not repeat the behavior that lead to the ban?**: ${appealData.future}
+
+        **Is there anything else you would like to add?**: ${appealData.extra}
+        `;
+      await messageModThread(
+        null,
+        botMember,
+        targetUser,
+        'BAN_APPEAL',
+        '',
+        description,
+        '',
+        '',
+        appealData,
+      );
+      return res.json(result);
     }
-  } else {
     return next();
+  } catch (error) {
+    return next(error);
   }
 });
 
 router.get('/:userId', async (req, res, next) => {
-  if (await checkAuth(req, res)) {
-    const { userId } = req.params;
-    log.debug(F, `userId: ${userId}`);
-    try {
-      if (userId === 'error') throw new Error('error');
-      const result = await appeals.getAppeals(userId);
-      if (result) {
-        return res.json(result);
-      }
-      return next();
-    } catch (error) {
-      return next(error);
+  const { userId } = req.params;
+  log.debug(F, `userId: ${userId}`);
+  try {
+    if (userId === 'error') throw new Error('error');
+    const result = await appeals.getAppeals(userId);
+    if (result) {
+      return res.json(result);
     }
-  } else {
     return next();
+  } catch (error) {
+    return next(error);
   }
 });
 
 router.get('/', async (req, res) => {
-  if (await checkAuth(req, res)) {
-    const result = await appeals.getAllAppeals();
-    res.json(result);
-  }
+  const result = await appeals.getAllAppeals();
+  res.json(result);
 });
 
 export default router;
